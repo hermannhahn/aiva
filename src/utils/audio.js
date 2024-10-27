3673ff05a6136f8a9083928ddd9390e8bfe3f550
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-27 12:07:21 -0300

    0.0.1

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,13 +1,7 @@
 import GLib from 'gi://GLib';
 import Gio from 'gi://Gio';
 
-import {Utils} from './utils.js';
-
 import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
-
-// Utils
-const utils = new Utils();
-const log = utils.log;
 
 export class Audio {
     constructor(app) {
@@ -21,13 +15,13 @@ export class Audio {
         this.playingPid = 0;
         this.pipeline = null;
         this.questionPath = null;
-        console.log('Audio loaded');
+        this.app.utils.log('Audio loaded');
     }
 
     // Play audio
     play(audiofile) {
         if (!this.isPlaying) {
-            log('Playing audio... ' + audiofile);
+            this.app.utils.log('Playing audio... ' + audiofile);
             // Process sync, not async
             const process = GLib.spawn_async(
                 null, // pasta de trabalho
@@ -39,12 +33,12 @@ export class Audio {
             if (process) {
                 this.playingPid = process.pid;
                 this.isPlaying = true;
-                log('Audio played successfully.');
+                this.app.utils.log('Audio played successfully.');
             } else {
-                log('Error playing audio.');
+                this.app.utils.log('Error playing audio.');
             }
         } else {
-            log('Audio already playing.');
+            this.app.utils.log('Audio already playing.');
             this.stop();
             this.play(audiofile);
         }
@@ -53,15 +47,17 @@ export class Audio {
     // Stop audio
     stop() {
         if (!this.isPlaying) {
-            log('Audio not playing.');
+            this.app.utils.log('Audio not playing.');
             return;
         }
         this.isPlaying = false;
-        log('Stopping audio...');
+        this.app.utils.log('Stopping audio...');
 
         // Kill player pid
         GLib.spawn_command_line_async('kill ' + this.playingPid);
-        log('Audio stopped successfully. [PID: ' + this.playingPid + ']');
+        this.app.utils.log(
+            'Audio stopped successfully. [PID: ' + this.playingPid + ']',
+        );
     }
 
     // Start record
@@ -69,11 +65,11 @@ export class Audio {
         if (this.isRecording) {
             // Stop recording
             this.stopRecord();
-            log('Recording stopped.');
+            this.app.utils.log('Recording stopped.');
             return;
         }
         this.isRecording = true;
-        log('Recording...');
+        this.app.utils.log('Recording...');
 
         // Definir o arquivo de saída no diretório da extensão
         this.questionPath = 'gva_temp_audio_XXXXXX.wav';
@@ -102,15 +98,15 @@ export class Audio {
     // Stop record
     stopRecord() {
         if (!this.isRecording) {
-            log('Recording not started.');
+            this.app.utils.log('Recording not started.');
             return;
         }
         this.isRecording = false;
-        log('Stopping recording...');
+        this.app.utils.log('Stopping recording...');
 
         // Stop recording
         this.pipeline.force_exit();
-        log('Recording stopped successfully.');
+        this.app.utils.log('Recording stopped successfully.');
 
         // Transcribe audio
         this.app.azure.transcribe(this.questionPath);
@@ -123,7 +119,7 @@ export class Audio {
             const [, contents] = file.load_contents(null);
             return GLib.base64_encode(contents);
         } catch (error) {
-            log('Erro ao ler o arquivo: ' + error);
+            this.app.utils.log('Erro ao ler o arquivo: ' + error);
             return null;
         }
     }
