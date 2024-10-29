cfec8c53a65fb8622e664ad92a80bc1b7025d121
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-29 15:59:05 -0300

    0.0.1

--- src/extension.js
+++ src/extension.js
@@ -285,6 +285,7 @@ const Aiva = GObject.registerClass(
 
             responseChat.label.clutter_text.line_wrap = true;
             responseChat.label.clutter_text.justify = true;
+            responseChat.actor.set_vexpand(true);
             // responseChat.label.clutter_text.selected_text_color = '#000000';
             // responseChat.label.clutter_text.selection_color = '#000000';
             // responseChat.label.clutter_text.selected_background_color =
