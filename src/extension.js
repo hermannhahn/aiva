16abcad69d2ab7f9eb38951b15868bd0eec2eaf8
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-29 01:06:48 -0300

    0.0.1

--- src/extension.js
+++ src/extension.js
@@ -216,7 +216,6 @@ const Aiva = GObject.registerClass(
             });
             inputChat.label.clutter_text.reactive = true;
             inputChat.label.clutter_text.selectable = true;
-            inputChat.label.clutter_text.hover = false;
             this.ui.inputChat = inputChat;
 
             // Response
@@ -225,7 +224,6 @@ const Aiva = GObject.registerClass(
             });
             responseChat.label.clutter_text.reactive = true;
             responseChat.label.clutter_text.selectable = true;
-            responseChat.label.clutter_text.hover = true;
             this.ui.responseChat = responseChat;
 
             // Copy Button
