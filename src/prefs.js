4e33fad3c6ef22531a48e7c336dab4560719e8f2
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-31 17:19:05 -0300

    0.0.1

--- src/prefs.js
+++ src/prefs.js
@@ -736,9 +736,9 @@ class GeminiSettings {
         this.main.attach(histroyLabel, 0, 6, 1, 1);
         this.main.attach(historyButton, 2, 6, 1, 1);
 
-        this.main.attach(save, 2, 7, 1, 1);
+        this.main.attach(save, 4, 7, 1, 1);
 
-        this.main.attach(statusLabel, 2, 8, 1, 1);
+        this.main.attach(statusLabel, 3, 8, 1, 1);
 
         // Adicionar a grade à página
         this.ui.add(this.main);
