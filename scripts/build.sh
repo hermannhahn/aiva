93d8eedf24a59910352d41f93d4a3cbb28f03744
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-31 16:09:35 -0300

    0.0.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -153,8 +153,8 @@ function try_restarting_gnome_shell() {
 }
 
 function install_extension_package() {
-	echo "Installing the extension $UUID..."
-	gnome-extensions install --force $UUID.shell-extension.zip
+	echo "Installing the extension..."
+	gnome-extensions install --force "$UUID".shell-extension.zip
 	echo "Extension installed."
 
 	if [ "$1" = "-r" ]; then
