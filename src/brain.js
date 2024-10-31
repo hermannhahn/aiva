835d7314a184e0bb03d3357256407eadb31e8634
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-31 20:59:03 -0300

    0.0.1

--- src/brain.js
+++ src/brain.js
@@ -8,4 +8,38 @@ export class Brain {
     proccess(question) {
         this.app.response(question);
     }
+
+    _isCommand(text) {
+        if (text.startsWith('/')) {
+            return true;
+        }
+        return false;
+    }
+
+    _isVoiceCommand(text) {
+        text = text.toLowerCase();
+        // Regex: please and open in same prhase
+        let please = _(
+            'please',
+        )
+        let activationWords = [
+            _(
+                'open',
+            ),
+            _('run'),
+            _('execute'),
+            _('start'),
+            _('launch'),
+            _('activate'),
+            _('close'),
+            _('stop'),
+            _('terminate'),
+            _('deactivate'),
+        ]
+        let regexs = [/please\s+open\s+in/g;
+        if (text.includes('voice')) {
+            return true;
+        }
+        return false;
+    }
 }
