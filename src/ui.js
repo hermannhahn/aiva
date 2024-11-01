5ae048327a94d476e0f049a4717a31e65ee1e549
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-31 23:35:02 -0300

    0.0.1

--- src/ui.js
+++ src/ui.js
@@ -2,8 +2,8 @@ import St from 'gi://St';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 export class AppLayout {
-    constructor() {
-        this.init();
+    constructor(app) {
+        this.app = app;
     }
 
     init() {
@@ -86,6 +86,72 @@ export class AppLayout {
         // Separator
         this.newSeparator = new PopupMenu.PopupSeparatorMenuItem();
 
-        return this;
+        this.createTray();
+    }
+
+    createTray() {
+        // Icon tray
+        this.ui.tray.add_child(this.ui.icon);
+        this.add_child(this.ui.tray);
+        this.log('App tray initialized.');
+
+        this.addItems();
+    }
+
+    addItems() {
+        // Add items container to menu
+        this.menu.addMenuItem(this.ui.item);
+        this.menu.style_class = 'menu';
+
+        // Add scrollview to menu box
+        this.menu.box.add_child(this.ui.scrollView);
+        this.menu.box.style_class = 'menu-box';
+
+        // Add chat to scrollbar
+        this.ui.scrollView.add_child(this.ui.chatSection.actor);
+
+        // Add search entry, mic button, clear button and settings button to items container
+        this.ui.item.add_child(this.ui.searchEntry);
+        this.ui.item.add_child(this.ui.micButton);
+        this.ui.item.add_child(this.ui.clearButton);
+        this.ui.item.add_child(this.ui.settingsButton);
+
+        this.itemsActions();
+    }
+
+    itemsActions() {
+        //
+        // Actions
+        //
+
+        // If press enter on question input box
+        this.ui.searchEntry.clutter_text.connect('activate', (actor) => {
+            const question = actor.text;
+            this.ui.searchEntry.clutter_text.set_text('');
+            this.ui.searchEntry.clutter_text.reactive = false;
+            this.chat(question);
+        });
+
+        // If press mic button
+        this.ui.micButton.connect('clicked', (_self) => {
+            this.audio.record();
+        });
+
+        // If press clear button
+        this.ui.clearButton.connect('clicked', (_self) => {
+            this.ui.searchEntry.clutter_text.set_text('');
+            this.chatHistory = [];
+            this.ui.menu.box.remove_child(this.ui.scrollView);
+            this.ui.chatSection = new PopupMenu.PopupMenuSection();
+            this.ui.scrollView.add_child(this.ui.chatSection.actor);
+            this.ui.menu.box.add_child(this.ui.scrollView);
+        });
+
+        // If press settings button
+        this.ui.settingsButton.connect('clicked', (_self) => {
+            this.openSettings();
+            // Close App
+            this.menu.close();
+        });
     }
 }
