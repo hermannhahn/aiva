9654929be8cf70febd697c9bf91c32e633583923
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-31 21:49:53 -0300

    0.0.1

--- src/brain.js
+++ src/brain.js
@@ -25,18 +25,34 @@ export class Brain {
     _isVoiceCommand(
         text,
         maxWords = 10,
-        triggerWord = 'please',
+        triggerWord = [_('computer')],
         activationWords = [
-            'open',
-            'run',
-            'execute',
-            'start',
-            'launch',
-            'activate',
-            'close',
-            'stop',
-            'terminate',
-            'exit',
+            _('open'),
+            _('start'),
+            _('launch'),
+            _('activate'),
+            _('close'),
+            _('stop'),
+            _('terminate'),
+            _('exit'),
+            _('run'),
+            _('execute'),
+            _('play'),
+            _('watch'),
+            _('search'),
+            _('find'),
+            _('read'),
+
+            // 'open',
+            // 'run',
+            // 'execute',
+            // 'start',
+            // 'launch',
+            // 'activate',
+            // 'close',
+            // 'stop',
+            // 'terminate',
+            // 'exit',
         ],
     ) {
         // Converter para minúsculas apenas uma vez
@@ -44,6 +60,15 @@ export class Brain {
 
         // Dividir o texto em palavras e pegar as primeiras 'maxWords'
         const words = text.split(/\s+/).slice(0, maxWords);
+        const activationWordInText = words.some((word) =>
+            activationWords.includes(word),
+        );
+        this.app.log(
+            'activationWordInText: ' +
+                activationWordInText +
+                ' words: ' +
+                words,
+        );
 
         // Verificar se 'triggerWord' e pelo menos uma 'activationWord' estão presentes
         return (
