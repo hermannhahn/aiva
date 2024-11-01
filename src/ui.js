62f6be7d8786742e9fff6dce75f0b8985bcbaa11
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-31 23:57:53 -0300

    0.0.1

--- src/ui.js
+++ src/ui.js
@@ -93,28 +93,28 @@ export class AppLayout {
 
     createTray() {
         // Icon tray
-        this.ui.tray.add_child(this.ui.icon);
-        this.add_child(this.ui.tray);
+        this.tray.add_child(this.icon);
+        this.app.add_child(this.tray);
         this.log('App tray initialized.');
     }
 
     addItems() {
         // Add items container to menu
-        this.menu.addMenuItem(this.ui.item);
-        this.menu.style_class = 'menu';
+        this.app.menu.addMenuItem(this.item);
+        this.app.menu.style_class = 'menu';
 
         // Add scrollview to menu box
-        this.menu.box.add_child(this.ui.scrollView);
-        this.menu.box.style_class = 'menu-box';
+        this.app.menu.box.add_child(this.scrollView);
+        this.app.menu.box.style_class = 'menu-box';
 
         // Add chat to scrollbar
-        this.ui.scrollView.add_child(this.ui.chatSection.actor);
+        this.scrollView.add_child(this.chatSection.actor);
 
         // Add search entry, mic button, clear button and settings button to items container
-        this.ui.item.add_child(this.ui.searchEntry);
-        this.ui.item.add_child(this.ui.micButton);
-        this.ui.item.add_child(this.ui.clearButton);
-        this.ui.item.add_child(this.ui.settingsButton);
+        this.item.add_child(this.searchEntry);
+        this.item.add_child(this.micButton);
+        this.item.add_child(this.clearButton);
+        this.item.add_child(this.settingsButton);
     }
 
     itemsActions() {
@@ -123,33 +123,33 @@ export class AppLayout {
         //
 
         // If press enter on question input box
-        this.ui.searchEntry.clutter_text.connect('activate', (actor) => {
+        this.searchEntry.clutter_text.connect('activate', (actor) => {
             const question = actor.text;
-            this.ui.searchEntry.clutter_text.set_text('');
-            this.ui.searchEntry.clutter_text.reactive = false;
-            this.chat(question);
+            this.searchEntry.clutter_text.set_text('');
+            this.searchEntry.clutter_text.reactive = false;
+            this.app.brain.proccess(question);
         });
 
         // If press mic button
-        this.ui.micButton.connect('clicked', (_self) => {
-            this.audio.record();
+        this.micButton.connect('clicked', (_self) => {
+            this.app.audio.record();
         });
 
         // If press clear button
-        this.ui.clearButton.connect('clicked', (_self) => {
-            this.ui.searchEntry.clutter_text.set_text('');
-            this.chatHistory = [];
-            this.ui.menu.box.remove_child(this.ui.scrollView);
-            this.ui.chatSection = new PopupMenu.PopupMenuSection();
-            this.ui.scrollView.add_child(this.ui.chatSection.actor);
-            this.ui.menu.box.add_child(this.ui.scrollView);
+        this.clearButton.connect('clicked', (_self) => {
+            this.searchEntry.clutter_text.set_text('');
+            this.app.chatHistory = [];
+            this.app.menu.box.remove_child(this.scrollView);
+            this.chatSection = new PopupMenu.PopupMenuSection();
+            this.scrollView.add_child(this.chatSection.actor);
+            this.app.menu.box.add_child(this.scrollView);
         });
 
         // If press settings button
-        this.ui.settingsButton.connect('clicked', (_self) => {
+        this.settingsButton.connect('clicked', (_self) => {
             this.openSettings();
             // Close App
-            this.menu.close();
+            this.app.menu.close();
         });
     }
 }
