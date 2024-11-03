b1b69072ba3f653441f2c8d49eb4e64ec152c10a
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-03 14:11:03 -0300

    0.0.1

--- src/chat.js
+++ src/chat.js
@@ -27,43 +27,28 @@ export class Chat {
 
     add(text, role = 'user') {
         if (role === 'user') {
+            let inputChat = this.app.ui.input();
             this.app.ui.inputChat.label.clutter_text.set_markup(
                 `<b>${this.app.userSettings.USERNAME}:</b> ${text}`,
             );
         } else {
+            let responseChat = this.app.ui.response();
+            let copyButton = this.app.ui.copy();
+
+            // add items
+            this.app.ui.chatSection.addMenuItem(inputChat);
+            this.app.ui.chatSection.addMenuItem(responseChat);
+            this.app.ui.chatSection.addMenuItem(copyButton);
+            this.app.ui.chatSection.addMenuItem(this.app.ui.newSeparator);
             this.app.ui.responseChat.label.clutter_text.set_markup(
                 `<b>${this.app.userSettings.ASSIST_NAME}:</b> ${text}`,
             );
         }
-    }
-
-    /**
-     *
-     * @param {*} userQuestion
-     *
-     * send question to chat
-     */
-    send(userQuestion) {
-        let inputChat = this.app.ui.input();
-        let responseChat = this.app.ui.response();
-        let copyButton = this.app.ui.copy();
-
-        // add items
-        this.app.ui.chatSection.addMenuItem(inputChat);
-        this.app.ui.chatSection.addMenuItem(responseChat);
-        this.app.ui.chatSection.addMenuItem(copyButton);
-        this.app.ui.chatSection.addMenuItem(this.app.ui.newSeparator);
 
         // add copy button
         copyButton.connect('activate', (_self) => {
             this.app.utils.copySelectedText(responseChat, copyButton);
         });
-
-        // Add user question to chat
-        this.addInput(userQuestion);
-
-        // Set temporary response message
-        this.addResponse('...');
 
         // Unlock search entry
         this.app.ui.searchEntry.clutter_text.reactive = true;
