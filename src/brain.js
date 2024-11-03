771f74d47da9e1e6d6eb350250319bad5d175f3f
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-03 20:37:12 -0300

    0.0.1

--- src/brain.js
+++ src/brain.js
@@ -3,6 +3,7 @@ import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js'
 export class Brain {
     constructor(app) {
         this.app = app;
+        this.app.log('Brain loaded.');
     }
 
     proccess(question) {
