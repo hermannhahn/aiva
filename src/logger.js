df15506a11458fb7dff014a18a7afc4fc740d6f9
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-03 17:09:38 -0300

    0.0.1

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,18 @@
 export class Logger {
+    /**
+     *
+     * @param {*} message
+     */
     log(message) {
         if (message) {
             console.log(`[AIVA] ${message}`);
         }
     }
 
+    /**
+     *
+     * @param {*} message
+     */
     logError(message) {
         if (message) {
             console.log(`[AIVA][ERROR] ${message}`);
