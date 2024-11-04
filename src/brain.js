ea5a6a909a1a7587b6297c102fb63504af28cc94
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-03 23:39:58 -0300

    0.0.1

--- src/brain.js
+++ src/brain.js
@@ -85,12 +85,23 @@ HELP
     _getSoftwareName(text) {
         // Get software name
         const softwareOptions = [
-            'google',
-            'chrome',
-            'firefox',
-            'youtube',
-            'vscode',
-            'code editor',
+            _('google'),
+            _('chrome'),
+            _('firefox'),
+            _('vscode'),
+            _('code editor'),
+            _('youtube'),
+            _('gmail'),
+            _('outlook'),
+            _('maps'),
+            _('calculator'),
+            _('calendar'),
+            _('notepad'),
+            _('snap store'),
+            _('extensions manager'),
+            _('settings'),
+            _('emulator'),
+            _('terminal'),
         ];
 
         // Check if software name is in softwareOptions
@@ -122,6 +133,45 @@ HELP
             case 'code editor':
                 this.app.utils.executeCommand('code');
                 break;
+            case 'gmail':
+                this.app.utils.executeCommand('google-chrome gmail.com');
+                break;
+            case 'outlook':
+                this.app.utils.executeCommand('google-chrome outlook.com');
+                break;
+            case 'maps':
+                this.app.utils.executeCommand('google-chrome maps.google.com');
+                break;
+            case 'calculator':
+                this.app.utils.executeCommand('google-chrome calculator.com');
+                break;
+            case 'calendar':
+                this.app.utils.executeCommand(
+                    'google-chrome calendar.google.com',
+                );
+                break;
+            case 'notepad':
+                this.app.utils.executeCommand('google-chrome notepad.com');
+                break;
+            case 'snap store':
+                this.app.utils.executeCommand('google-chrome snap.shop');
+                break;
+            case 'extensions manager':
+                this.app.utils.executeCommand(
+                    'google-chrome extensions.gnome.org',
+                );
+                break;
+            case 'settings':
+                this.app.openSettings();
+                break;
+            case 'emulator':
+                this.app.utils.executeCommand(
+                    'google-chrome emulator.google.com',
+                );
+                break;
+            case 'terminal':
+                this.app.utils.executeCommand('gnome-terminal');
+                break;
             default:
                 this.app.log('Software not found');
                 break;
@@ -129,15 +179,16 @@ HELP
     }
 
     _voiceCommandInterpreter(text) {
-        // Converter para minúsculas apenas uma vez
         text = text.toLowerCase();
-
         const activationWordInText = this._activationWord(text);
-
-        // Verificar se a palavra de ativação está no texto
         if (activationWordInText) {
             // If command is open
-            if (activationWordInText === _('open')) {
+            if (
+                activationWordInText === _('open') ||
+                activationWordInText === _('start') ||
+                activationWordInText === _('launch') ||
+                activationWordInText === _('run')
+            ) {
                 const softwareName = this._getSoftwareName(text);
 
                 if (softwareName) {
