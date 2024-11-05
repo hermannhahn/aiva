e7ed21b10e2c7ba16e8f3c207d7c8ebed30099e1
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-05 00:30:24 -0300

    0.0.1

--- src/actions/open.js
+++ src/actions/open.js
@@ -9,7 +9,7 @@ export class Open {
             `My boss asked me to open an application. The computer's operating system is Linux Ubuntu. Interpret the following sentence and respond with only one command line so that I can run it on his computer's terminal and achieve the objective of the request. Consider that the computer may not have the application and it will be necessary to include the installation in the command line. Rules for responding: Respond with only one command line. Solicitation: ${text}`,
         );
         if (commandToRun === false) {
-            this.app.azure.tts(
+            this.app.chat.add(
                 _('Sorry, I can not open this application right now.'),
             );
             return;
