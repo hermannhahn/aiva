3e361a3e96b439449d4366152dd3ddf3fdb772ed
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-05 15:42:22 -0300

    0.0.1

--- src/interpreter.js
+++ src/interpreter.js
@@ -64,16 +64,19 @@ HELP
     _voiceCommandInterpreter(text) {
         let request = text.toLowerCase();
         this.app.gemini.runCommand(`
-Please, folow the instructions.
-Objective: Fulfill the request using a command line in the linux ubuntu terminal.
-Request: ${request}
-Response format: json
-Request example 1: "computador pesquise por travessia balsa santos"
-Response example 1: {success: true, response: 'Pesquisando por travessia balsa santos...', commandline: 'firefox https://www.google.com/search?q=travessia+balsa+santos'}
-Request example 2: "computador, remova o fundo dessa imagem"
-Response example 2: {success: true, response: 'Não consigo remover o fundo para você, porém você pode fazer isso utilizando aplicativos ou sites. Um exemplo é o site canvas. Envie a imagem que deseja remover o fundo no site que abri.', commandline: 'firefox https://www.canva.com/pt_br/recursos/remover-fundo/'}
-Request example 3: "computador, aplique o filtro de blur na imagem"
-Response example 3: {success: false, response: 'Não consigo fazer isso para você, porém você pode utilizar o photoshop. Siga as instruções a seguir para aplicar o filtro de blur na imagem pelo photoshop. Instruções: Abra....', commandline: null}
+Please, folow the instructions below.\n
+Objective: Fulfill the request using a command line to be executed in the linux ubuntu terminal.\n
+Request: ${request}\n
+Response type: string\n
+How to response examples: \n
+Request example 1: "computador pesquise por travessia balsa santos"\n
+Response example 1: "{success: true, response: 'Pesquisando por travessia balsa santos...', commandline: 'firefox https://www.google.com/search?q=travessia+balsa+santos'}"\n
+Request example 2: "computador, remova o fundo dessa imagem"\n
+Response example 2: "{success: true, response: 'Não consigo remover o fundo para você, porém você pode fazer isso utilizando aplicativos ou sites. Um exemplo é o site canvas. Envie a imagem que deseja remover o fundo no site que abri.', commandline: 'firefox https://www.canva.com/pt_br/recursos/remover-fundo/'}"\n
+Request example 3: "computador, aplique o filtro de blur na imagem"\n
+Response example 3: "{success: false, response: 'Não consigo fazer isso para você, porém você pode utilizar o photoshop. Siga as instruções a seguir para aplicar o filtro de blur na imagem pelo photoshop. Instruções: Abra....', commandline: null}"\n
+Request example 4: "computador pesquise por videos de gatos no youtube"\n
+Response example 4: "{success: true, response: 'Pesquisando por videos de gatos no youtube...', commandline: 'firefox https://www.youtube.com/results?search_query=videos+de+gatos'}"\n
 `);
     }
 }
