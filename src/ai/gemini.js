4a83e7ac380f15f9f2a0c9e4a753ba05eb24efae
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-05 00:18:03 -0300

    0.0.1

--- src/ai/gemini.js
+++ src/ai/gemini.js
@@ -182,6 +182,7 @@ export class GoogleGemini {
                     }
                     let aiResponse = res.candidates[0]?.content?.parts[0]?.text;
                     this.app.log('Success getting response.');
+                    this.app.log(aiResponse);
                     return aiResponse;
                 },
             );
