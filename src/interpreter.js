c8871a0caf2a548b5d8ca3b9eadb69d99623a468
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-05 10:06:58 -0300

    0.0.1

--- src/interpreter.js
+++ src/interpreter.js
@@ -62,52 +62,55 @@ HELP
     }
 
     _voiceCommandInterpreter(text) {
-        text = text.toLowerCase();
-        const maxWords = 10;
-        const openAppWords = [
-            _('open'),
-            'abre',
-            'abra',
-            _('start'),
-            'inicie',
-            'inicia',
-            _('launch'),
-            _('run'),
-            'rode',
-            'roda',
-            _('execute'),
-            'execute',
-            'executa',
-        ];
-        const closeAppWords = [
-            _('close'),
-            'feche',
-            'fecha',
-            _('terminate'),
-            'termine',
-            'termina',
-            'encerra',
-            'encerre',
-            _('exit'),
-            'saie',
-            'saia',
-            _('kill'),
-            'mate',
-            'mata',
-        ];
+        let solicitation = text.toLowerCase();
+        // const maxWords = 10;
+        // const openAppWords = [
+        //     _('open'),
+        //     'abre',
+        //     'abra',
+        //     _('start'),
+        //     'inicie',
+        //     'inicia',
+        //     _('launch'),
+        //     _('run'),
+        //     'rode',
+        //     'roda',
+        //     _('execute'),
+        //     'execute',
+        //     'executa',
+        // ];
+        // const closeAppWords = [
+        //     _('close'),
+        //     'feche',
+        //     'fecha',
+        //     _('terminate'),
+        //     'termine',
+        //     'termina',
+        //     'encerra',
+        //     'encerre',
+        //     _('exit'),
+        //     'saie',
+        //     'saia',
+        //     _('kill'),
+        //     'mate',
+        //     'mata',
+        // ];
         // Dividir o texto em palavras e pegar as primeiras 'maxWords'
-        const words = text.split(/\s+/).slice(0, maxWords);
-        for (const word of words) {
-            if (openAppWords.includes(word)) {
-                this.app.gemini.runCommand(
-                    `My boss asked me to open an application. The computer's operating system is Linux Ubuntu. Interpret the following solicitation and respond with only one command line so that I can run it on his computer's terminal and achieve the objective of the request. Consider that the computer have the application the command line to run. Rules for responding: Respond with only one command line. Solicitation: ${text}`,
-                );
-                break;
-            } else if (closeAppWords.includes(word)) {
-                this.close.software(text);
-            } else {
-                this.app.log('Command not found');
-            }
-        }
+        // const words = text.split(/\s+/).slice(0, maxWords);
+        // for (const word of words) {
+        //     if (openAppWords.includes(word)) {
+        this.app.gemini.runCommand(
+            `My boss asked me to do something in his computer. The computer's operating system is Linux Ubuntu. Interpret the following solicitation and respond with only one command line to do what he needs,so that I can run it on his computer's terminal and achieve the objective of the request. Consider that the computer have the application to run. Rules for responding: Respond only with command line to do what he needs. If the solicitation it is not possible to do in terminal respond only with word "false". Solicitation: ${solicitation}`,
+        );
+        // this.app.gemini.runCommand(
+        //     `My boss asked me to open an application. The computer's operating system is Linux Ubuntu. Interpret the following solicitation and respond with only one command line so that I can run it on his computer's terminal and achieve the objective of the request. Consider that the computer have the application the command line to run. Rules for responding: Respond with only one command line. Solicitation: ${text}`,
+        // );
+        //     break;
+        // } else if (closeAppWords.includes(word)) {
+        //     this.close.software(text);
+        // } else {
+        //     this.app.log('Command not found');
+        // }
+        // }
     }
 }
