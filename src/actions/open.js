0c4e2c214f11430c610cabeeac2b430eafacdf7c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-05 00:08:14 -0300

    0.0.1

--- src/actions/open.js
+++ src/actions/open.js
@@ -4,112 +4,15 @@ export class Open {
     }
 
     app(text) {
-        const softwareName = this._getSoftwareName(text);
-        if (softwareName) {
-            this.app.log('Opening: ' + softwareName);
-            this.app.chat.add(`Opening: ${softwareName}`);
-            this._runSoftware(softwareName);
-        } else {
-            this.app.log('Software not found');
-        }
-    }
-
-    _getSoftwareName(text) {
-        // Get software name
-        const softwareOptions = [
-            _('google'),
-            _('chrome'),
-            _('firefox'),
-            _('vscode'),
-            _('code editor'),
-            _('youtube'),
-            _('gmail'),
-            _('outlook'),
-            _('maps'),
-            _('calculator'),
-            _('calendar'),
-            _('notepad'),
-            _('snap store'),
-            _('extensions manager'),
-            _('settings'),
-            _('emulator'),
-            _('terminal'),
-        ];
-
-        // Check if software name is in softwareOptions
-        const maxWords = 10;
-        const words = text.split(/\s+/).slice(0, maxWords);
-        const softwareName = words.find((word) =>
-            softwareOptions.includes(word),
+        // Ask to gemini how to open what user want to open
+        this.app.azure.tts('Opening...');
+        const commandToRun = this.app.gemini.commandLine(
+            `My boss asked me to open an application. The computer's operating system is Linux Ubuntu. Interpret the following sentence and respond with only one command line so that I can run it on his computer's terminal and achieve the objective of the request. Consider that the computer may not have the application and it will be necessary to include the installation in the command line. Rules for responding: Respond with only one command line. Solicitation: ${text}`,
         );
-        return softwareName;
-    }
-
-    _runSoftware(software) {
-        switch (software) {
-            case _('google'):
-                this.app.interpreter.pids.push({
-                    name: 'google-chrome',
-                    pid: this.app.utils.executeCommand('google-chrome'),
-                });
-                break;
-            case _('chrome'):
-                this.app.utils.executeCommand('google-chrome');
-                break;
-            case _('firefox'):
-                this.app.utils.executeCommand('firefox');
-                break;
-            case _('youtube'):
-                this.app.utils.executeCommand('firefox youtube.com');
-                break;
-            case _('vscode'):
-                this.app.utils.executeCommand('code');
-                break;
-            case _('code editor'):
-                this.app.utils.executeCommand('code');
-                break;
-            case _('gmail'):
-                this.app.utils.executeCommand('google-chrome gmail.com');
-                break;
-            case _('outlook'):
-                this.app.utils.executeCommand('google-chrome outlook.com');
-                break;
-            case _('maps'):
-                this.app.utils.executeCommand('google-chrome maps.google.com');
-                break;
-            case _('calculator'):
-                this.app.utils.executeCommand('google-chrome calculator.com');
-                break;
-            case _('calendar'):
-                this.app.utils.executeCommand(
-                    'google-chrome calendar.google.com',
-                );
-                break;
-            case _('notepad'):
-                this.app.utils.executeCommand('google-chrome notepad.com');
-                break;
-            case _('snap store'):
-                this.app.utils.executeCommand('google-chrome snap.shop');
-                break;
-            case _('extensions manager'):
-                this.app.utils.executeCommand(
-                    'google-chrome extensions.gnome.org',
-                );
-                break;
-            case _('settings'):
-                this.app.openSettings();
-                break;
-            case _('emulator'):
-                this.app.utils.executeCommand(
-                    'google-chrome emulator.google.com',
-                );
-                break;
-            case _('terminal'):
-                this.app.utils.executeCommand('gnome-terminal');
-                break;
-            default:
-                this.app.log('Software not found');
-                break;
+        if (!this.app.utils.executeCommand(commandToRun)) {
+            this.app.azure.tts(
+                _('Sorry, I can not open this application right now.'),
+            );
         }
     }
 }
