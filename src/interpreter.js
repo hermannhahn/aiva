a708a29d40485615bb828b66d46024719db7bf49
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-05 14:58:20 -0300

    0.0.1

--- src/interpreter.js
+++ src/interpreter.js
@@ -63,42 +63,6 @@ HELP
 
     _voiceCommandInterpreter(text) {
         let request = text.toLowerCase();
-        // const maxWords = 10;
-        // const openAppWords = [
-        //     _('open'),
-        //     'abre',
-        //     'abra',
-        //     _('start'),
-        //     'inicie',
-        //     'inicia',
-        //     _('launch'),
-        //     _('run'),
-        //     'rode',
-        //     'roda',
-        //     _('execute'),
-        //     'execute',
-        //     'executa',
-        // ];
-        // const closeAppWords = [
-        //     _('close'),
-        //     'feche',
-        //     'fecha',
-        //     _('terminate'),
-        //     'termine',
-        //     'termina',
-        //     'encerra',
-        //     'encerre',
-        //     _('exit'),
-        //     'saie',
-        //     'saia',
-        //     _('kill'),
-        //     'mate',
-        //     'mata',
-        // ];
-        // Dividir o texto em palavras e pegar as primeiras 'maxWords'
-        // const words = text.split(/\s+/).slice(0, maxWords);
-        // for (const word of words) {
-        //     if (openAppWords.includes(word)) {
         this.app.gemini.runCommand(`
 Please, folow the instructions.
 Objective: Fulfill the request using a command line in the linux ubuntu terminal.
@@ -111,15 +75,5 @@ Response example 2: {success: true, response: 'Não consigo remover o fundo para
 Request example 3: "computador, aplique o filtro de blur na imagem"
 Response example 3: {success: false, response: 'Não consigo fazer isso para você, porém você pode utilizar o photoshop. Siga as instruções a seguir para aplicar o filtro de blur na imagem pelo photoshop. Instruções: Abra....', commandline: null}
 `);
-        // this.app.gemini.runCommand(
-        //     `My boss asked me to open an application. The computer's operating system is Linux Ubuntu. Interpret the following solicitation and respond with only one command line so that I can run it on his computer's terminal and achieve the objective of the request. Consider that the computer have the application the command line to run. Rules for responding: Respond with only one command line. Solicitation: ${text}`,
-        // );
-        //     break;
-        // } else if (closeAppWords.includes(word)) {
-        //     this.close.software(text);
-        // } else {
-        //     this.app.log('Command not found');
-        // }
-        // }
     }
 }
