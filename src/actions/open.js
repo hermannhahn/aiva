21705774364d1c608d6fc68ab9a0f7a952bf7f05
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-05 00:31:25 -0300

    0.0.1

--- src/actions/open.js
+++ src/actions/open.js
@@ -8,6 +8,7 @@ export class Open {
         const commandToRun = this.app.gemini.commandLine(
             `My boss asked me to open an application. The computer's operating system is Linux Ubuntu. Interpret the following sentence and respond with only one command line so that I can run it on his computer's terminal and achieve the objective of the request. Consider that the computer may not have the application and it will be necessary to include the installation in the command line. Rules for responding: Respond with only one command line. Solicitation: ${text}`,
         );
+        this.app.log(commandToRun);
         if (commandToRun === false) {
             this.app.chat.add(
                 _('Sorry, I can not open this application right now.'),
