1a9ba24e815661fe5c252a2aa96eb67d17b5ab2c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-07 17:53:20 -0300

    0.0.1

--- src/chat.js
+++ src/chat.js
@@ -1,14 +1,22 @@
 import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
+/**
+ * @description manage chat
+ * @param {*} app
+ */
 export class Chat {
     constructor(app) {
         this.app = app;
-        // load history
         this.history = [];
         this.app.log('Chat loaded.');
     }
 
+    /**
+     * @description initialize chat
+     */
     init() {
+        this.app.log('Initializing chat...');
+
         // if recursive talk is enabled
         if (this.app.userSettings.RECURSIVE_TALK) {
             // load history file
@@ -17,10 +25,16 @@ export class Chat {
         } else {
             this.app.log('Recursive talk is disabled.');
         }
+
         this.app.log('Chat initialized.');
     }
 
-    add(text, voice = true) {
+    /**
+     * @description add text to the chat
+     * @param {*} text - text to add
+     * @param {boolean} [speech=true] - speech text
+     */
+    add(text, speech = true) {
         let chat = this.app.ui.chat();
         this.app.ui.chatSection.addMenuItem(chat);
         chat.label.clutter_text.set_markup(
@@ -28,13 +42,18 @@ export class Chat {
         );
         this.app.ui.responseChat = chat;
         this.app.ui.searchEntry.clutter_text.reactive = true;
-        if (voice) {
+        if (speech) {
             this.app.azure.tts(text);
         }
         this.app.utils.scrollToBottom();
     }
 
-    addQuestion(text, voice = false) {
+    /**
+     * @description add question to the chat
+     * @param {*} text - text to add
+     * @param {boolean} [speech=true] - speech text
+     */
+    addQuestion(text, speech = false) {
         const inputChat = this.app.ui.question();
         this.app.ui.chatSection.addMenuItem(inputChat);
         text = this.app.utils.inputformat(text);
@@ -43,25 +62,35 @@ export class Chat {
         );
         this.app.ui.inputChat = inputChat;
         this.app.ui.searchEntry.clutter_text.reactive = false;
-        if (voice) {
+        if (speech) {
             this.app.azure.tts(text);
         }
         this.app.utils.scrollToBottom();
     }
 
-    editQuestion(text, voice = false) {
+    /**
+     * @description edit last question
+     * @param {*} text - new text for the question
+     * @param {boolean} [speech=true] - speech text
+     */
+    editQuestion(text, speech = false) {
         let formatedText = this.app.utils.inputformat(text);
         this.app.ui.inputChat.label.clutter_text.set_markup(
             `<b>${this.app.userSettings.USERNAME}:</b> ${formatedText}`,
         );
         this.app.ui.searchEntry.clutter_text.reactive = false;
-        if (voice) {
+        if (speech) {
             this.app.azure.tts(text);
         }
         this.app.utils.scrollToBottom();
     }
 
-    addResponse(text, voice = false) {
+    /**
+     * @description add response to the chat
+     * @param {*} text - text to add
+     * @param {boolean} [speech=true] - speech text
+     */
+    addResponse(text, speech = false) {
         let responseChat = this.app.ui.response();
         let copyButton = this.app.ui.copy();
         this.app.ui.chatSection.addMenuItem(responseChat);
@@ -77,19 +106,24 @@ export class Chat {
         copyButton.connect('activate', (_self) => {
             this.app.utils.copySelectedText(responseChat, copyButton);
         });
-        if (voice) {
+        if (speech) {
             this.app.azure.tts(text);
         }
         this.app.utils.scrollToBottom();
     }
 
-    editResponse(text, voice = true) {
+    /**
+     * @description edit last response
+     * @param {*} text - new text for the response
+     * @param {boolean} [speech=true] - speech text
+     */
+    editResponse(text, speech = true) {
         const formatedText = this.app.utils.insertLineBreaks(text);
         this.app.ui.responseChat.label.clutter_text.set_markup(
             `<b>${this.app.userSettings.ASSIST_NAME}:</b> ${formatedText}`,
         );
         this.app.ui.searchEntry.clutter_text.reactive = true;
-        if (voice === true) {
+        if (speech) {
             this.app.azure.tts(text);
         }
         this.app.utils.scrollToBottom();
