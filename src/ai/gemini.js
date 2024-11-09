c43e3ea0305fd943b3d1e77b2c4a9a23184721f9
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-09 16:07:53 -0300

    0.0.1

--- src/ai/gemini.js
+++ src/ai/gemini.js
@@ -258,6 +258,27 @@ export class GoogleGemini {
         return instructions;
     }
 
+    getCommandString(request) {
+        const response = `
+"${_('For the request')}": "${request}"
+${_('Return a JSON with the following keys')}:
+'success' (${_('true if it can be executed')} ${_('on a Linux Ubuntu terminal')}, ${_('false otherwise')}),
+'response' (${_('text to be spoken, informing success, failure or action')}) ${_('e')}
+'commandline' (${_('command to be executed in the terminal')}).
+${_('Rules for commandline value')}: ${_('Do not use sudo')}, ${_('assume any application is already installed')}.
+${_('Examples of responses')}:
+${_('Request')}: "${_('computer search for santos ferry crossing')}"
+${_('JSON Response')}: "{success: true, response: '${_('Searching for santos ferry crossing...')}', commandline: 'firefox https://www.google.com/search?q=${_('ferry+crossing+santos')}'}"
+${_('Request')}: "${_('search for cat videos on youtube')}"
+${_('JSON Response')}: "{success: true, response: '${_('Searching for cat videos on youtube...')}', commandline: 'firefox firefox https://www.youtube.com/results?search_query=${_('cat')}'}"
+${_('Request')}: "${_('remove the background of this image')}"
+${_('JSON Response')}: "{success: true, response: '${_("I can't remove the background for you")}, ${_('but you can do it using apps or websites.')}. ${_('One example is the canvas website.')}. ${_("I'm opening it for you.")}.', commandline: 'firefox https://www.canva.com/pt_br/recursos/remover-fundo/'}"
+${_('Request')}: "${_('apply the blur filter to the image')}"
+${_('JSON Response')}: "{success: false, response: '${_("I can't do that for you")}, ${_('but you can use photoshop.')}. ${_('Follow the instructions below to apply')} ${_('the blur filter to the image using photoshop')}. ${_('Instructions: Open...')}', commandline: null}"
+`;
+        return response;
+    }
+
     /**
      * @description build body for request
      * @param {string} text
