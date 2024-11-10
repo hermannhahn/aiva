52325d9f8db4fc66f44a8de73eb096dc83126762
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-10 04:09:05 -0300

    0.0.1

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -222,6 +222,8 @@ export class Utils {
         return new Promise((resolve, reject) => {
             const lang = this.app.userSettings.AZURE_SPEECH_LANGUAGE;
             const countryLang = lang.split('-')[1];
+            this.app.log('Language: ' + lang);
+            this.app.log('Country: ' + countryLang);
             const url = `https://news.google.com/rss?hl=${lang}&gl=${countryLang}&ceid=${countryLang}`;
             let session = new Soup.Session();
             let message = Soup.Message.new('GET', url);
