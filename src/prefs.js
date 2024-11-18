6b58cc09c61c260ea7fe836501c8e7537ac555f2
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-18 19:13:25 -0300

    0.0.1

--- src/prefs.js
+++ src/prefs.js
@@ -35,7 +35,7 @@ class AivaSettings {
             title: '⚙ ' + _('SETTINGS'),
         });
 
-        this.settingsContainer = new Gtk.Grid({
+        this.ui = new Gtk.Grid({
             margin_top: 10,
             margin_bottom: 10,
             margin_start: 10,
@@ -763,35 +763,35 @@ class AivaSettings {
         });
 
         // Add to grid
-        this.settingsContainer.attach(apiKeyLabel, 0, 0, 1, 1);
-        this.settingsContainer.attach(apiKeyIcon, 1, 0, 1, 1);
-        this.settingsContainer.attach(apiKey, 2, 0, 1, 1);
-        this.settingsContainer.attach(howToApiKey, 3, 0, 1, 1);
-        this.settingsContainer.attach(speechKeyLabel, 0, 1, 1, 1);
-        this.settingsContainer.attach(speechKeyIcon, 1, 1, 1, 1);
-        this.settingsContainer.attach(speechKey, 2, 1, 1, 1);
-        this.settingsContainer.attach(howToSpeechKey, 3, 1, 1, 1);
-        this.settingsContainer.attach(speechRegionLabel, 0, 2, 1, 1);
-        this.settingsContainer.attach(speechRegionIcon, 1, 2, 1, 1);
-        this.settingsContainer.attach(speechRegion, 2, 2, 1, 1);
-        this.settingsContainer.attach(sysLanguageLabel, 0, 3, 1, 1);
-        this.settingsContainer.attach(sysLanguageIcon, 1, 3, 1, 1);
-        this.settingsContainer.attach(languageSelector, 2, 3, 1, 1);
-        this.settingsContainer.attach(voiceLabel, 0, 4, 1, 1);
-        this.settingsContainer.attach(voiceIcon, 1, 4, 1, 1);
-        this.settingsContainer.attach(voiceSelector, 2, 4, 1, 1);
-        this.settingsContainer.attach(assistNameLabel, 0, 5, 1, 1);
-        this.settingsContainer.attach(assistNameIcon, 1, 5, 1, 1);
-        this.settingsContainer.attach(assistName, 2, 5, 1, 1);
-        this.settingsContainer.attach(histroyIcon, 1, 6, 1, 1);
-        this.settingsContainer.attach(history, 2, 6, 1, 1);
-        this.settingsContainer.attach(histroyLabel, 2, 6, 1, 1);
-        this.settingsContainer.attach(blankLine, 0, 7, 3, 1);
-        this.settingsContainer.attach(save, 0, 8, 3, 1);
-        this.settingsContainer.attach(statusLabel, 0, 9, 3, 1);
+        this.ui.attach(apiKeyLabel, 0, 0, 1, 1);
+        this.ui.attach(apiKeyIcon, 1, 0, 1, 1);
+        this.ui.attach(apiKey, 2, 0, 1, 1);
+        this.ui.attach(howToApiKey, 3, 0, 1, 1);
+        this.ui.attach(speechKeyLabel, 0, 1, 1, 1);
+        this.ui.attach(speechKeyIcon, 1, 1, 1, 1);
+        this.ui.attach(speechKey, 2, 1, 1, 1);
+        this.ui.attach(howToSpeechKey, 3, 1, 1, 1);
+        this.ui.attach(speechRegionLabel, 0, 2, 1, 1);
+        this.ui.attach(speechRegionIcon, 1, 2, 1, 1);
+        this.ui.attach(speechRegion, 2, 2, 1, 1);
+        this.ui.attach(sysLanguageLabel, 0, 3, 1, 1);
+        this.ui.attach(sysLanguageIcon, 1, 3, 1, 1);
+        this.ui.attach(languageSelector, 2, 3, 1, 1);
+        this.ui.attach(voiceLabel, 0, 4, 1, 1);
+        this.ui.attach(voiceIcon, 1, 4, 1, 1);
+        this.ui.attach(voiceSelector, 2, 4, 1, 1);
+        this.ui.attach(assistNameLabel, 0, 5, 1, 1);
+        this.ui.attach(assistNameIcon, 1, 5, 1, 1);
+        this.ui.attach(assistName, 2, 5, 1, 1);
+        this.ui.attach(histroyIcon, 1, 6, 1, 1);
+        this.ui.attach(history, 2, 6, 1, 1);
+        this.ui.attach(histroyLabel, 2, 6, 1, 1);
+        this.ui.attach(blankLine, 0, 7, 3, 1);
+        this.ui.attach(save, 0, 8, 3, 1);
+        this.ui.attach(statusLabel, 0, 9, 3, 1);
 
         // Add to main
-        this.main.add(this.settingsContainer);
+        this.main.add(this.ui);
     }
 
     translations(text, lang) {
