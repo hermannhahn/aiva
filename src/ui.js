80ab018c799424a6b019ab7802f4e53204e3862f
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-20 00:37:28 -0300

    0.0.1

--- src/ui.js
+++ src/ui.js
@@ -251,7 +251,7 @@ export class UI {
             // get menu box parent
             // show appearance box
             if (parent) {
-                parent.insert_child_above(this.appearanceBox, this.app.menu);
+                parent.insert_child_above(this.appearanceBox, this.app.menu.box);
                 this.appearanceBox.add_child(this.transparencyLabel);
                 this.appearanceBox.add_child(this.transparencyEntry);
                 this.appearanceBox.add_child(this.transparencyButton);
