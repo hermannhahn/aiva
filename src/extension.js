56f54ffd42f4788f7dbaa458e40efb25317d0c04
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-20 14:38:15 -0300

    0.0.1

--- src/extension.js
+++ src/extension.js
@@ -83,7 +83,7 @@ const Aiva = GObject.registerClass(
                 LOCATION: this._getLocation(),
                 RECURSIVE_TALK: settings.get_boolean('log-history'),
                 USERNAME: GLib.get_real_name(),
-                TRANSPARENCY: settings.get_int('transparency'),
+                TRANSPARENCY: settings.get_string('theme-transparency'),
             };
         }
 
