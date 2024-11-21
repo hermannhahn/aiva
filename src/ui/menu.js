4348282167c6c7849f94a43608caf0699f592911
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-21 15:37:11 -0300

    0.0.1

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -1,13 +1,12 @@
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 export class Menu {
-    add() {
-        // Create app item section
-        const item = new PopupMenu.PopupBaseMenuItem({
+    constructor() {
+        this.menu = new PopupMenu.PopupBaseMenuItem({
             style_class: 'app-items',
             reactive: false,
             can_focus: true,
         });
-        return item;
+        return this.menu;
     }
 }
