9de08951e5cf3d94eff00b9000bc5cdac90e33c1
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-21 15:35:55 -0300

    0.0.1

--- src/ui/main.js
+++ src/ui/main.js
@@ -6,9 +6,11 @@ export class Main {
         const tray = new St.BoxLayout({
             style_class: 'system-tray',
         });
+        // Create icon
         const icon = new St.Icon({
             style_class: 'tray-icon',
         });
+        // Return tray and icon
         return {tray, icon};
     }
 }
