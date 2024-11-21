9572407ad873925b90a1f44d856bd73abebfeae6
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-21 14:46:40 -0300

    0.0.1

--- src/ui/main.js
+++ src/ui/main.js
@@ -1,6 +1,18 @@
+import St from 'gi://St';
+
 export class Main {
     constructor(app) {
         this.app = app;
         this.app.log('Main loaded.');
+    }
+
+    add() {
+        // Create tray
+        this.app.ui.tray = new St.BoxLayout({
+            style_class: 'system-tray',
+        });
+        this.app.ui.icon = new St.Icon({
+            style_class: 'tray-icon',
+        });
     }
 }
