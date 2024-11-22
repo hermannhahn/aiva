532926e647f60c7d2ff26cbbb330ed69f68ec57f
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-22 01:31:13 -0300

    0.0.1

--- src/ui/chat.js
+++ src/ui/chat.js
@@ -43,4 +43,79 @@ export class Chat {
         this.scrollView.add_child(this.chatSection.actor);
         this.scrollView.set_style(`background-color: rgba(42, 42, 42, 0);`);
     }
+
+    /**
+     * @description create chat item
+     * @returns {object} chat item
+     */
+    chat() {
+        // Question
+        let chat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'input-chat',
+            can_focus: false,
+        });
+        let scolor = chat.label.clutter_text.get_selected_text_color();
+        this.app.log('Selection Color ' + scolor);
+        console.log('Selection Color ' + scolor);
+        chat.label.clutter_text.reactive = true;
+        chat.label.clutter_text.selectable = true;
+        chat.label.clutter_text.hover = true;
+        return chat;
+    }
+
+    /**
+     * @description create question item
+     * @returns {object} question item
+     */
+    question() {
+        this.setTheme(
+            this.app.userSettings.TRANSPARENCY,
+            this.app.userSettings.COLOR,
+        );
+        // Question
+        let inputChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'input-chat',
+            can_focus: false,
+        });
+        inputChat.label.clutter_text.reactive = true;
+        inputChat.label.clutter_text.selectable = true;
+        inputChat.label.clutter_text.hover = true;
+        this.inputChat = inputChat;
+        return inputChat;
+    }
+
+    /**
+     * @description create response item
+     * @returns {object} response item
+     */
+    response() {
+        // Response
+        let responseChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'response-chat',
+            can_focus: false,
+        });
+        responseChat.label.clutter_text.reactive = true;
+        responseChat.label.clutter_text.selectable = true;
+        responseChat.label.clutter_text.hover = true;
+        responseChat.label.clutter_text.justify = true;
+        this.responseChat = responseChat;
+        return responseChat;
+    }
+
+    /**
+     * @description create copy button item
+     * @returns {object} copy button item
+     */
+    copy() {
+        // Copy Button
+        let copyButton = new PopupMenu.PopupMenuItem('', {
+            style_class: 'copy-icon',
+            can_focus: false,
+        });
+        copyButton.label.clutter_text.reactive = true;
+        copyButton.label.clutter_text.selectable = true;
+        copyButton.label.clutter_text.hover = true;
+        this.copyButton = copyButton;
+        return copyButton;
+    }
 }
