7e9099ee9fe97c4f7c0a36682bb3750def99f90c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-21 22:47:32 -0300

    0.0.1

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -1,12 +1,84 @@
+import St from 'gi://St';
+
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 export class Menu {
     constructor() {
-        this.menu = new PopupMenu.PopupBaseMenuItem({
+        this.create();
+        this.app.log('Menu loaded.');
+    }
+
+    create() {
+        // Create interface
+        this.interface = new PopupMenu.PopupBaseMenuItem({
+            style_class: 'app-interface',
+            reactive: false,
+            can_focus: true,
+        });
+
+        // AI Character
+        this.character = new St.Button({
+            label: '🤖',
+            style_class: 'character-icon',
+            can_focus: false,
+        });
+
+        // User question entry
+        this.userEntry = new St.Entry({
+            style_class: 'user-entry',
+            hint_text: _('Ask me anything...'),
+            track_hover: true,
+            x_expand: true,
+            y_expand: true,
+            can_focus: true,
+        });
+
+        // Create enter button
+        this.enterButton = new St.Button({
+            label: '',
+            style_class: 'enter-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        // Create voice activation button
+        this.micButton = new St.Button({
+            label: '',
+            style_class: 'mic-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        // Create clear history button
+        this.clearButton = new St.Button({
+            label: '🗑️',
+            style_class: 'trash-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        // Create settings button
+        this.settingsButton = new St.Button({
+            label: '⚙️',
+            style_class: 'settings-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        this.appearanceMenu = new PopupMenu.PopupBaseMenuItem({
             style_class: 'app-items',
             reactive: false,
             can_focus: true,
         });
+
+        // Create appearance button
+        this.appearanceButton = new St.Button({
+            label: '🎨',
+            style_class: 'appearance-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
         return this.menu;
     }
 }
