dbe605ea247843b3eca26e0dba5eac158d4761a0
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-21 22:55:02 -0300

    0.0.1

--- src/ui/menu_colors.js
+++ src/ui/menu_colors.js
@@ -1,6 +1,100 @@
+import * as St from 'gi://St';
+import * as PopupMenu from 'gi://PopupMenu';
+
 export class MenuColors {
-    constructor(app) {
-        this.app = app;
+    constructor() {
+        this._create();
         this.app.log('MenuColors loaded.');
+        return this;
+    }
+
+    _create() {
+        this.appearanceMenu = new PopupMenu.PopupBaseMenuItem({
+            style_class: 'app-interface',
+            reactive: false,
+            can_focus: true,
+        });
+
+        // Create appearance box
+        this.appearanceBox = new St.BoxLayout({
+            style_class: 'appearance-box',
+        });
+
+        // Create transparency slider
+        this.transparencyEntry = new St.Entry({
+            style_class: 'transparency-entry',
+            can_focus: true,
+        });
+        this.transparencyEntry.clutter_text.set_text(
+            this.app.userSettings.TRANSPARENCY,
+        );
+
+        // Create transparency label
+        this.transparencyLabel = new St.Label({
+            text: 'Transparency: ',
+            style_class: 'transparency-label',
+        });
+
+        // Create transparency ok button
+        this.transparencyButton = new St.Button({
+            label: 'OK',
+            style_class: 'transparency-ok-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create colors label
+        this.colorsLabel = new St.Label({
+            text: 'Color: ',
+            style_class: 'colors-label',
+        });
+
+        // Create color blue button
+        this.colorBlueButton = new St.Button({
+            label: '🟦',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color red button
+        this.colorRedButton = new St.Button({
+            label: '🟥',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color green button
+        this.colorGreenButton = new St.Button({
+            label: '🟩',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color yellow button
+        this.colorYellowButton = new St.Button({
+            label: '🟨',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color purple button
+        this.colorPurpleButton = new St.Button({
+            label: '🟪',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color black button
+        this.colorBlackButton = new St.Button({
+            label: '⬛',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
     }
 }
