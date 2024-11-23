dc085faf657fca076eba2afcfa3809daa6ddd36f
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-22 21:40:49 -0300

    0.0.1

--- src/ui/interface.js
+++ src/ui/interface.js
@@ -1,10 +1,16 @@
 import St from 'gi://St';
 
+import {Menu} from './menu.js';
+import {Chat} from './chat.js';
+
 /**
  * @description app user interface
  */
 export class Interface {
     constructor() {
+        this.menu = new Menu();
+        this.chat = new Chat();
+
         // Create tray
         this.tray = new St.BoxLayout({
             style_class: 'system-tray',
