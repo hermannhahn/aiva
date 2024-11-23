f6ea40c531394c74f7497d9a96881a0d9adc3266
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-23 01:20:56 -0300

    0.0.1

--- src/ui.js
+++ src/ui.js
@@ -1,37 +1,500 @@
+import St from 'gi://St';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-import St from 'gi://St';
-
-import {Interface} from './interface.js';
-
 /**
- * @description user interface
+ * @description app user interface
  * @param {object} app
+ * @example
+ * instance:
+ * const ui = new UI(app);
+ *
+ * public function
+ * init() - initialize interfaces
+ * chat() - return chat item
+ * question() - return new question item
+ * response() - return new response item
+ * copy() - return new copy button item
+ *
+ * private function
+ * _createApp() - create app and tray
+ * _addItems() - add items to app
+ * _itemsActions() - event handlers
  */
 export class UI {
     constructor(app) {
-        // App
         this.app = app;
-        this.app.menu.style_class = 'app';
-        this.app.menu.box.style_class = 'app-box';
-
-        // Tray
-        this.tray = new St.BoxLayout({
-            style_class: 'tray',
-        });
-        this.icon = new St.Icon({
-            style_class: 'tray-icon',
-        });
-
-        this.interface = new Interface();
+        this.app.log('UI loaded.');
     }
 
     /**
-     * @description initialize app ui
+     * @description initialize interfaces
      */
-    create() {
-        this.app.add_child(this.tray);
+    init() {
+        this.app.log('Initializing UI...');
+
+        // Create tray
+        this.tray = new St.BoxLayout({
+            style_class: 'system-tray',
+        });
+        this.icon = new St.Icon({
+            style_class: 'google-assistant-icon',
+        });
+
+        // Create app item section
+        this.items = new PopupMenu.PopupBaseMenuItem({
+            style_class: 'app-items',
+            reactive: false,
+            can_focus: true,
+        });
+
+        // Status Icon
+        this.statusBar = new St.Button({
+            label: '🤖',
+            style_class: 'status-icon',
+            can_focus: false,
+        });
+
+        this.searchEntry = new St.Entry({
+            style_class: 'search-entry',
+            hint_text: _('Ask me anything...'),
+            track_hover: true,
+            x_expand: true,
+            y_expand: true,
+            can_focus: true,
+        });
+
+        // Create enter button
+        this.enterButton = new St.Button({
+            label: '',
+            style_class: 'enter-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        // Create voice activation button
+        this.micButton = new St.Button({
+            label: '',
+            style_class: 'mic-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        // Create clear history button
+        this.clearButton = new St.Button({
+            label: '🗑️',
+            style_class: 'trash-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        // Create settings button
+        this.settingsButton = new St.Button({
+            label: '⚙️',
+            style_class: 'settings-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        this.appearanceMenu = new PopupMenu.PopupBaseMenuItem({
+            style_class: 'app-items',
+            reactive: false,
+            can_focus: true,
+        });
+
+        // Create appearance button
+        this.appearanceButton = new St.Button({
+            label: '🎨',
+            style_class: 'appearance-icon',
+            toggle_mode: true,
+            can_focus: false,
+        });
+
+        // Create appearance box
+        this.appearanceBox = new St.BoxLayout({
+            style_class: 'appearance-box',
+        });
+
+        // Create transparency slider
+        this.transparencyEntry = new St.Entry({
+            style_class: 'transparency-entry',
+            can_focus: true,
+        });
+        this.transparencyEntry.clutter_text.set_text(
+            this.app.userSettings.TRANSPARENCY,
+        );
+
+        // Create transparency label
+        this.transparencyLabel = new St.Label({
+            text: 'Transparency: ',
+            style_class: 'transparency-label',
+        });
+
+        // Create transparency ok button
+        this.transparencyButton = new St.Button({
+            label: 'OK',
+            style_class: 'transparency-ok-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create colors label
+        this.colorsLabel = new St.Label({
+            text: 'Color: ',
+            style_class: 'colors-label',
+        });
+
+        // Create color blue button
+        this.colorBlueButton = new St.Button({
+            label: '🟦',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color red button
+        this.colorRedButton = new St.Button({
+            label: '🟥',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color green button
+        this.colorGreenButton = new St.Button({
+            label: '🟩',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color yellow button
+        this.colorYellowButton = new St.Button({
+            label: '🟨',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color purple button
+        this.colorPurpleButton = new St.Button({
+            label: '🟪',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create color black button
+        this.colorBlackButton = new St.Button({
+            label: '⬛',
+            style_class: 'colors-icon',
+            toggle_mode: true,
+            can_focus: true,
+        });
+
+        // Create scrollbar
+        this.scrollView = new St.ScrollView({
+            style_class: 'chat-scroll',
+            overlay_scrollbars: false,
+            can_focus: false,
+        });
+
+        // Create chat section
+        this.chatSection = new PopupMenu.PopupMenuSection({
+            style_class: 'chat-section',
+            can_focus: false,
+        });
+
+        // Create input and response chat items
+        this.inputChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'input-chat',
+            can_focus: false,
+        });
+        this.responseChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'response-chat',
+            can_focus: false,
+        });
+
+        // Create copy button
+        this.copyButton = new PopupMenu.PopupMenuItem('', {
+            style_class: 'copy-icon',
+            can_focus: false,
+        });
+
+        // Separator
+        this.newSeparator = new PopupMenu.PopupSeparatorMenuItem();
+
+        // Initialize
+        this._createApp();
+        this._addItems();
+        this._itemsActions();
+
+        this.app.log('UI initialized.');
+    }
+
+    /**
+     * @description create app tray
+     */
+    _createApp() {
+        // Icon tray
         this.tray.add_child(this.icon);
-        this.interface.create(this.app.menu);
+        this.app.add_child(this.tray);
+        this.app.log('App tray initialized.');
+    }
+
+    /**
+     * @description add items to app tray
+     */
+    _addItems() {
+        // Add items container to app
+        this.app.menu.addMenuItem(this.appearanceMenu);
+        this.app.menu.addMenuItem(this.items);
+        this.app.menu.style_class = 'app';
+        this.app.menu.box.style_class = 'app-box';
+
+        // Add items
+        this.items.add_child(this.statusBar);
+        this.items.add_child(this.searchEntry);
+        this.items.add_child(this.enterButton);
+        this.items.add_child(this.micButton);
+        this.items.add_child(this.clearButton);
+        this.items.add_child(this.settingsButton);
+        this.items.add_child(this.appearanceButton);
+
+        // Add scrollview
+        this.app.menu.box.add_child(this.scrollView);
+
+        // Add chat to scrollbar
+        this.scrollView.add_child(this.chatSection.actor);
+
+        // Apply userSettings appearance
+        this.setTheme(
+            this.app.userSettings.TRANSPARENCY,
+            this.app.userSettings.COLOR,
+        );
+        this.scrollView.set_style(`background-color: rgba(42, 42, 42, 0);`);
+    }
+
+    /**
+     * @description items actions
+     */
+    _itemsActions() {
+        // If press enter on question input box
+        this.searchEntry.clutter_text.connect('activate', (actor) => {
+            const question = actor.text;
+            this.searchEntry.clutter_text.set_text('');
+            this.searchEntry.clutter_text.reactive = false;
+            this.app.chat.addQuestion(question);
+            this.app.interpreter.proccess(question);
+        });
+
+        // If press mic button
+        this.micButton.connect('clicked', (_self) => {
+            this.app.audio.record();
+        });
+
+        // If press clear button
+        this.clearButton.connect('clicked', (_self) => {
+            this.searchEntry.clutter_text.set_text('');
+            this.app.chat.history = [];
+            this.app.menu.box.remove_child(this.scrollView);
+            this.chatSection = new PopupMenu.PopupMenuSection();
+            this.scrollView.add_child(this.chatSection.actor);
+            this.app.menu.box.add_child(this.scrollView);
+        });
+
+        // If press settings button
+        this.settingsButton.connect('clicked', (_self) => {
+            this.app.openSettings();
+            // Close App
+            if (this.appearanceBoxIsOpen) {
+                this.appearanceMenu.remove_child(this.appearanceBox);
+                this.appearanceBoxIsOpen = false;
+            }
+            this.app.menu.close();
+        });
+
+        // If press appearance button
+        this.appearanceBoxIsOpen = false;
+        this.appearanceButton.connect('clicked', (_self) => {
+            if (this.appearanceBoxIsOpen) {
+                this.appearanceMenu.remove_child(this.appearanceBox);
+                this.appearanceBoxIsOpen = false;
+                return;
+            }
+            this.appearanceBoxIsOpen = true;
+            // get menu box parent
+            // show appearance box
+            this.appearanceMenu.add_child(this.appearanceBox);
+            // Add transparency slider
+            this.appearanceBox.add_child(this.transparencyLabel);
+            this.appearanceBox.add_child(this.transparencyEntry);
+            this.appearanceBox.add_child(this.transparencyButton);
+            // Add appearance buttons to appearance box
+            this.appearanceBox.add_child(this.colorsLabel);
+            this.appearanceBox.add_child(this.colorBlackButton);
+            this.appearanceBox.add_child(this.colorBlueButton);
+            this.appearanceBox.add_child(this.colorRedButton);
+            this.appearanceBox.add_child(this.colorGreenButton);
+            this.appearanceBox.add_child(this.colorYellowButton);
+            this.appearanceBox.add_child(this.colorPurpleButton);
+
+            this.transparencyButton.connect('clicked', (_self) => {
+                const transparency = this.transparencyEntry.clutter_text.text;
+                if (transparency === '' || isNaN(parseInt(transparency))) {
+                    return;
+                }
+                this.setTheme(transparency, this.app.userSettings.COLOR);
+            });
+
+            // Connect color buttons
+            this.colorBlueButton.connect('clicked', () => {
+                this.setTheme(this.app.userSettings.TRANSPARENCY, '0, 0, 100');
+            });
+            this.colorRedButton.connect('clicked', () => {
+                this.setTheme(this.app.userSettings.TRANSPARENCY, '100, 0, 0');
+            });
+            this.colorGreenButton.connect('clicked', () => {
+                this.setTheme(this.app.userSettings.TRANSPARENCY, '0, 100, 0');
+            });
+            this.colorYellowButton.connect('clicked', () => {
+                this.setTheme(
+                    this.app.userSettings.TRANSPARENCY,
+                    '100, 100, 0',
+                );
+            });
+            this.colorPurpleButton.connect('clicked', () => {
+                this.setTheme(
+                    this.app.userSettings.TRANSPARENCY,
+                    '100, 0, 100',
+                );
+            });
+            this.colorBlackButton.connect('clicked', () => {
+                this.setTheme(this.app.userSettings.TRANSPARENCY, '54, 54, 54');
+            });
+        });
+    }
+
+    /**
+     * @description create chat item
+     * @returns {object} chat item
+     */
+    chat() {
+        // Question
+        let chat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'input-chat',
+            can_focus: false,
+        });
+        let scolor = chat.label.clutter_text.get_selected_text_color();
+        this.app.log('Selection Color ' + scolor);
+        console.log('Selection Color ' + scolor);
+        chat.label.clutter_text.reactive = true;
+        chat.label.clutter_text.selectable = true;
+        chat.label.clutter_text.hover = true;
+        return chat;
+    }
+
+    /**
+     * @description create question item
+     * @returns {object} question item
+     */
+    question() {
+        this.setTheme(
+            this.app.userSettings.TRANSPARENCY,
+            this.app.userSettings.COLOR,
+        );
+        // Question
+        let inputChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'input-chat',
+            can_focus: false,
+        });
+        inputChat.label.clutter_text.reactive = true;
+        inputChat.label.clutter_text.selectable = true;
+        inputChat.label.clutter_text.hover = true;
+        this.inputChat = inputChat;
+        return inputChat;
+    }
+
+    /**
+     * @description create response item
+     * @returns {object} response item
+     */
+    response() {
+        // Response
+        let responseChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'response-chat',
+            can_focus: false,
+        });
+        responseChat.label.clutter_text.reactive = true;
+        responseChat.label.clutter_text.selectable = true;
+        responseChat.label.clutter_text.hover = true;
+        responseChat.label.clutter_text.justify = true;
+        this.responseChat = responseChat;
+        return responseChat;
+    }
+
+    /**
+     * @description create copy button item
+     * @returns {object} copy button item
+     */
+    copy() {
+        // Copy Button
+        let copyButton = new PopupMenu.PopupMenuItem('', {
+            style_class: 'copy-icon',
+            can_focus: false,
+        });
+        copyButton.label.clutter_text.reactive = true;
+        copyButton.label.clutter_text.selectable = true;
+        copyButton.label.clutter_text.hover = true;
+        this.copyButton = copyButton;
+        return copyButton;
+    }
+
+    statusIcon(icon) {
+        this.statusBar.label = icon;
+        return true;
+    }
+
+    resetStatusIcon() {
+        this.statusBar.label = '🤖';
+        return true;
+    }
+
+    setTheme(transparency, color) {
+        // set default if empty, null or undefined
+        if (
+            transparency === '' ||
+            transparency === null ||
+            transparency === undefined
+        ) {
+            transparency = '75';
+        }
+        if (color === '' || color === null || color === undefined) {
+            color = '54, 54, 54';
+        }
+        // set transparencyEntry text
+        this.transparencyEntry.clutter_text.set_text(transparency);
+
+        // save
+        const tString = transparency.toString();
+        this.app.extension.settings.set_string('theme-transparency', tString);
+        this.app.userSettings.TRANSPARENCY = tString;
+        this.app.extension.settings.set_string('theme-color', color);
+        this.app.userSettings.COLOR = color;
+        // set theme
+        transparency = 100 - transparency;
+        transparency = parseInt(transparency) / 100;
+        this.items.set_style(
+            `background-color: rgba(${color}, ${transparency});`,
+        );
+        this.scrollView.set_style(
+            `background-color: rgba(${color}, ${transparency});`,
+        );
+        if (transparency < 0.8) {
+            transparency += 0.2;
+        }
+        this.searchEntry.set_style(
+            `background-color: rgba(${color}, ${transparency});`,
+        );
     }
 }
