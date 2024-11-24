087d21a5f8672c4afd794d6a68d99ac9dcf99c4f
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-24 17:31:01 -0300

    0.0.1

--- src/chat.js
+++ src/chat.js
@@ -64,8 +64,9 @@ export class Chat {
      * @param {boolean} [speech=true] - speech text
      */
     addQuestion(text, speech = false) {
-        this.app.ui.chat.container.set_style(
-            `background-color: rgba(${this.app.userSettings.COLOR}, ${this.app.userSettings.TRANSPARENCY});`,
+        this.app.ui.setTheme(
+            this.app.userSettings.TRANSPARENCY,
+            this.app.userSettings.COLOR,
         );
         const inputChat = this.app.ui.chat.question();
         this.app.ui.chat.box.addMenuItem(inputChat);
