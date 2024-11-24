16e003b4fcdd22f5c7eb61a02b2cadedfb9b330a
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-24 01:13:06 -0300

    0.0.1

--- src/ui.js
+++ src/ui.js
@@ -26,6 +26,10 @@ export class UI {
     init() {
         // Initialize
         this._createApp();
+        this.setTheme(
+            this.app.userSettings.TRANSPARENCY,
+            this.app.userSettings.COLOR,
+        );
         this._addItems();
         this._itemsActions();
     }
@@ -49,16 +53,6 @@ export class UI {
         this.container.addMenuItem(this.appearancemenu.container);
         this.container.addMenuItem(this.mainmenu.container);
         this.container.box.add_child(this.chat.container);
-
-        // Add chat to scrollbar
-        this.chat.container.add_child(this.chat.box.actor);
-
-        // Apply userSettings appearance
-        this.setTheme(
-            this.app.userSettings.TRANSPARENCY,
-            this.app.userSettings.COLOR,
-        );
-        this.scrollView.set_style(`background-color: rgba(42, 42, 42, 0);`);
     }
 
     /**
@@ -66,183 +60,97 @@ export class UI {
      */
     _itemsActions() {
         // If press enter on question input box
-        this.userEntry.clutter_text.connect('activate', (actor) => {
+        this.mainmenu.userEntry.clutter_text.connect('activate', (actor) => {
             const question = actor.text;
-            this.userEntry.clutter_text.set_text('');
-            this.userEntry.clutter_text.reactive = false;
+            this.mainmenu.userEntry.clutter_text.set_text('');
+            this.mainmenu.userEntry.clutter_text.reactive = false;
             this.app.chat.addQuestion(question);
             this.app.interpreter.proccess(question);
         });
 
         // If press mic button
-        this.micButton.connect('clicked', (_self) => {
+        this.mainmenu.micButton.connect('clicked', (_self) => {
             this.app.audio.record();
         });
 
         // If press clear button
-        this.clearButton.connect('clicked', (_self) => {
-            this.userEntry.clutter_text.set_text('');
+        this.mainmenu.clearButton.connect('clicked', (_self) => {
+            this.mainmenu.userEntry.clutter_text.set_text('');
             this.app.chat.history = [];
-            this.container.box.remove_child(this.scrollView);
-            this.chatSection = new PopupMenu.PopupMenuSection();
-            this.scrollView.add_child(this.chatSection.actor);
-            this.container.box.add_child(this.scrollView);
+            this.container.box.remove_child(this.chat.container);
+            this.chat.box = new PopupMenu.PopupMenuSection();
+            this.chat.container.add_child(this.chat.box.actor);
+            this.container.box.add_child(this.chat.container);
         });
 
         // If press settings button
-        this.settingsButton.connect('clicked', (_self) => {
+        this.mainmenu.settingsButton.connect('clicked', (_self) => {
             this.app.openSettings();
             // Close App
-            if (this.appearanceBoxIsOpen) {
-                this.appearanceMenu.remove_child(this.appearanceBox);
-                this.appearanceBoxIsOpen = false;
+            if (this.appearancemenu.isOpen) {
+                this.container.remove_child(this.appearancemenu.container);
+                this.appearancemenu.isOpen = false;
             }
             this.container.close();
         });
 
         // If press appearance button
-        this.appearanceBoxIsOpen = false;
+        this.appearancemenu.isOpen = false;
         this.appearanceButton.connect('clicked', (_self) => {
-            if (this.appearanceBoxIsOpen) {
-                this.appearanceMenu.remove_child(this.appearanceBox);
-                this.appearanceBoxIsOpen = false;
+            // show or hide
+            if (this.appearancemenu.isOpen) {
+                this.appearancemenu.hide();
                 return;
             }
-            this.appearanceBoxIsOpen = true;
-            // get menu box parent
-            // show appearance box
-            this.appearanceMenu.add_child(this.appearanceBox);
-            // Add transparency slider
-            this.appearanceBox.add_child(this.transparencyLabel);
-            this.appearanceBox.add_child(this.transparencyEntry);
-            this.appearanceBox.add_child(this.transparencyButton);
-            // Add appearance buttons to appearance box
-            this.appearanceBox.add_child(this.colorsLabel);
-            this.appearanceBox.add_child(this.colorBlackButton);
-            this.appearanceBox.add_child(this.colorBlueButton);
-            this.appearanceBox.add_child(this.colorRedButton);
-            this.appearanceBox.add_child(this.colorGreenButton);
-            this.appearanceBox.add_child(this.colorYellowButton);
-            this.appearanceBox.add_child(this.colorPurpleButton);
+            this.appearancemenu.show();
 
-            this.transparencyButton.connect('clicked', (_self) => {
-                const transparency = this.transparencyEntry.clutter_text.text;
-                if (transparency === '' || isNaN(parseInt(transparency))) {
-                    return;
-                }
-                this.setTheme(transparency, this.app.userSettings.COLOR);
-            });
+            this.appearancemenu.transparencyButton.connect(
+                'clicked',
+                (_self) => {
+                    const transparency =
+                        this.appearancemenu.transparencyEntry.clutter_text.text;
+                    if (transparency === '' || isNaN(parseInt(transparency))) {
+                        return;
+                    }
+                    this.setTheme(transparency, this.app.userSettings.COLOR);
+                },
+            );
 
             // Connect color buttons
-            this.colorBlueButton.connect('clicked', () => {
+            this.appearancemenu.colorBlueButton.connect('clicked', () => {
                 this.setTheme(this.app.userSettings.TRANSPARENCY, '0, 0, 100');
             });
-            this.colorRedButton.connect('clicked', () => {
+            this.appearancemenu.colorRedButton.connect('clicked', () => {
                 this.setTheme(this.app.userSettings.TRANSPARENCY, '100, 0, 0');
             });
-            this.colorGreenButton.connect('clicked', () => {
+            this.appearancemenu.colorGreenButton.connect('clicked', () => {
                 this.setTheme(this.app.userSettings.TRANSPARENCY, '0, 100, 0');
             });
-            this.colorYellowButton.connect('clicked', () => {
+            this.appearancemenu.colorYellowButton.connect('clicked', () => {
                 this.setTheme(
                     this.app.userSettings.TRANSPARENCY,
                     '100, 100, 0',
                 );
             });
-            this.colorPurpleButton.connect('clicked', () => {
+            this.appearancemenu.colorPurpleButton.connect('clicked', () => {
                 this.setTheme(
                     this.app.userSettings.TRANSPARENCY,
                     '100, 0, 100',
                 );
             });
-            this.colorBlackButton.connect('clicked', () => {
+            this.appearancemenu.colorBlackButton.connect('clicked', () => {
                 this.setTheme(this.app.userSettings.TRANSPARENCY, '54, 54, 54');
             });
         });
     }
 
-    // /**
-    //  * @description create chat item
-    //  * @returns {object} chat item
-    //  */
-    // box() {
-    //     // Question
-    //     let chat = new PopupMenu.PopupMenuItem('', {
-    //         style_class: 'input-chat',
-    //         can_focus: false,
-    //     });
-    //     let scolor = chat.label.clutter_text.get_selected_text_color();
-    //     this.app.log('Selection Color ' + scolor);
-    //     console.log('Selection Color ' + scolor);
-    //     chat.label.clutter_text.reactive = true;
-    //     chat.label.clutter_text.selectable = true;
-    //     chat.label.clutter_text.hover = true;
-    //     return chat;
-    // }
-
-    /**
-     * @description create question item
-     * @returns {object} question item
-     */
-    question() {
-        this.setTheme(
-            this.app.userSettings.TRANSPARENCY,
-            this.app.userSettings.COLOR,
-        );
-        // Question
-        let inputChat = new PopupMenu.PopupMenuItem('', {
-            style_class: 'input-chat',
-            can_focus: false,
-        });
-        inputChat.label.clutter_text.reactive = true;
-        inputChat.label.clutter_text.selectable = true;
-        inputChat.label.clutter_text.hover = true;
-        this.inputChat = inputChat;
-        return inputChat;
-    }
-
-    /**
-     * @description create response item
-     * @returns {object} response item
-     */
-    response() {
-        // Response
-        let responseChat = new PopupMenu.PopupMenuItem('', {
-            style_class: 'response-chat',
-            can_focus: false,
-        });
-        responseChat.label.clutter_text.reactive = true;
-        responseChat.label.clutter_text.selectable = true;
-        responseChat.label.clutter_text.hover = true;
-        responseChat.label.clutter_text.justify = true;
-        this.responseChat = responseChat;
-        return responseChat;
-    }
-
-    /**
-     * @description create copy button item
-     * @returns {object} copy button item
-     */
-    copy() {
-        // Copy Button
-        let copyButton = new PopupMenu.PopupMenuItem('', {
-            style_class: 'copy-icon',
-            can_focus: false,
-        });
-        copyButton.label.clutter_text.reactive = true;
-        copyButton.label.clutter_text.selectable = true;
-        copyButton.label.clutter_text.hover = true;
-        this.copyButton = copyButton;
-        return copyButton;
-    }
-
     statusIcon(icon) {
-        this.character.label = icon;
+        this.mainmenu.character.label = icon;
         return true;
     }
 
     resetStatusIcon() {
-        this.character.label = '🤖';
+        this.mainmenu.character.label = '🤖';
         return true;
     }
 
