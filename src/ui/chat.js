d6e99f7b6bf05809c42ac0f1a53621af8f181f28
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-24 17:08:07 -0300

    0.0.1

--- src/ui/chat.js
+++ src/ui/chat.js
@@ -57,7 +57,6 @@ export class Chat {
             can_focus: false,
         });
         let scolor = chat.label.clutter_text.get_selected_text_color();
-        this.app.log('Selection Color ' + scolor);
         console.log('Selection Color ' + scolor);
         chat.label.clutter_text.reactive = true;
         chat.label.clutter_text.selectable = true;
