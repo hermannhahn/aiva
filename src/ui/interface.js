1b485a7de8e9617472aa8047b477ec037f36133e
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-24 01:29:52 -0300

    0.0.1

--- src/ui/interface.js
+++ src/ui/interface.js
@@ -5,6 +5,10 @@ import St from 'gi://St';
  */
 export class Interface {
     constructor() {
+        this._create();
+    }
+
+    _create() {
         // Create tray
         this.tray = new St.BoxLayout({
             style_class: 'tray',
