daa78e5b3e5e116e3497825c476f30637c121936
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-24 22:15:47 -0300

    0.0.1

--- src/ui/appearancemenu.js
+++ src/ui/appearancemenu.js
@@ -20,7 +20,7 @@ export class AppearanceMenu {
             style_class: 'appearance-menu',
             reactive: false,
             can_focus: true,
-        })();
+        });
 
         // Create transparency slider
         this.transparencyEntry = new St.Entry({
