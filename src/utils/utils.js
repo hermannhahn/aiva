961b56a095afc223d449a37d54222b94d533774d
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-27 01:15:04 -0300

    0.0.1

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -674,7 +674,7 @@ export class Utils {
         return contents;
     };
 
-    darkColors(rgbString) {
+    lightColors(rgbString) {
         // Split the RGB string into its component parts.
         const [r, g, b] = rgbString.split(',');
 
@@ -694,7 +694,7 @@ export class Utils {
         return adjustedRgbString;
     }
 
-    lightColors(rgbString) {
+    darkColors(rgbString) {
         // Split the RGB string into its component parts.
         const [r, g, b] = rgbString.split(',');
 
