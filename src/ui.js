9648b24b817cd9a0c0e26027826b2af906fdb539
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-27 16:47:45 -0300

    0.0.1

--- src/ui.js
+++ src/ui.js
@@ -218,7 +218,7 @@ export class UI {
             `color: rgb(200, 200, 200);`,
         );
         this.appearancemenu.transparencyEntry.set_style(
-            `background-color: rgba(${lightnedColor}, 0.75);`,
+            `background-color: rgba(${lightnedColor});`,
         );
 
         // main menu
@@ -227,7 +227,7 @@ export class UI {
         );
         this.mainmenu.userEntry.set_style(`color: rgb(200, 200, 200);`);
         this.mainmenu.userEntry.set_style(
-            `background-color: rgba(${lightnedColor}, 0.75);`,
+            `background-color: rgba(${lightnedColor});`,
         );
 
         // chat
