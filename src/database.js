1de57ab70acd2257aa807ed244994d5e7f6e978d
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-28 14:43:39 -0300

    0.0.1

--- src/database.js
+++ src/database.js
@@ -10,7 +10,7 @@ class DatabaseManager {
     initDatabase() {
         this.db = Sqlite.open(this.dbPath);
 
-        // Criação da tabela "users" se não existir
+        // Create "users" if not exists
         const createUsersTableQuery = `
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,17 +20,35 @@ class DatabaseManager {
         `;
         this.db.exec(createUsersTableQuery);
 
-        // Criação da tabela "settings" se não existir
+        // Create "settings" if not exists
         const createSettingsTableQuery = `
             CREATE TABLE IF NOT EXISTS settings (
                 user_id INTEGER NOT NULL,
                 apikey TEXT,
                 speechkey TEXT,
-                location TEXT,
+                speechregion TEXT,
+                speechlanguage TEXT,
+                speechvoice TEXT,
+                assistname TEXT,
+                nickname TEXT,
+                savehistory BOOLEAN,
                 FOREIGN KEY(user_id) REFERENCES users(id)
             );
         `;
         this.db.exec(createSettingsTableQuery);
+
+        // Create "history" if not exists
+        const createHistoryTableQuery = `
+            CREATE TABLE IF NOT EXISTS history (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                question TEXT NOT NULL,
+                response TEXT NOT NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            );
+        `;
+        this.db.exec(createHistoryTableQuery);
     }
 
     addUser(user, email) {
@@ -63,20 +81,40 @@ class DatabaseManager {
         statement.finalize();
     }
 
-    addOrEditSetting(userId, apikey, speechkey, location) {
+    addOrEditSetting(
+        userId,
+        apikey,
+        speechkey,
+        speechregion,
+        speechlanguage,
+        speechvoice,
+        assistname,
+        nickname,
+        savehistory,
+    ) {
         const query = `
-            INSERT INTO settings (user_id, apikey, speechkey, location) 
-            VALUES (?, ?, ?, ?) 
+            INSERT INTO settings (user_id, apikey, speechkey, speechregion, speechlanguage, speechvoice, assistname, nickname, savehistory)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             ON CONFLICT(user_id) DO UPDATE SET
                 apikey = excluded.apikey,
                 speechkey = excluded.speechkey,
-                location = excluded.location;
+                speechregion = excluded.speechregion,
+                speechlanguage = excluded.speechlanguage,
+                speechvoice = excluded.speechvoice,
+                assistname = excluded.assistname,
+                nickname = excluded.nickname,
+                savehistory = excluded.savehistory;
         `;
         const statement = this.db.prepare(query);
         statement.bindInt(1, userId);
         statement.bindText(2, apikey);
         statement.bindText(3, speechkey);
-        statement.bindText(4, location);
+        statement.bindText(4, speechregion);
+        statement.bindText(5, speechlanguage);
+        statement.bindText(6, speechvoice);
+        statement.bindText(7, assistname);
+        statement.bindText(8, nickname);
+        statement.bindInt(9, savehistory);
         statement.step();
         statement.reset();
         statement.finalize();
@@ -105,12 +143,19 @@ const dbPath = GLib.build_filenamev([
     'my_extension',
     'database.sqlite',
 ]);
+
 const dbManager = new DatabaseManager(dbPath);
 dbManager.addUser('Hermann', 'hermann.h.hahn@gmail.com');
 dbManager.addOrEditSetting(
     1,
     'api_key_example',
     'speech_key_example',
+    'speech_region_example',
+    'speech_language_example',
+    'speech_voice_example',
+    'assist_name_example',
+    'nickname_example',
     'location_example',
+    true,
 );
 dbManager.closeDatabase();
