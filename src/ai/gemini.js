dd28308ab30604b6e90984476112805926116ec7
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-29 15:06:42 -0300

    0.0.1

--- src/ai/gemini.js
+++ src/ai/gemini.js
@@ -260,7 +260,7 @@ export class GoogleGemini {
 
     /**
      * @description create tune string
-     * @param {string} [role='user'] type the role or leave empty
+     * @param {string} [role='user'] type the role or leave empty for user role
      * @returns {string} tune text
      */
     getTuneString(role = 'user') {
