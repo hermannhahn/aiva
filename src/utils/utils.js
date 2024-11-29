459cf64b85792781f65f9b7379badf9ace696894
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-29 17:14:29 -0300

    0.0.1

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -432,19 +432,6 @@ export class Utils {
         );
     }
 
-    findCategoryInArrays(string, commands) {
-        for (const category in commands) {
-            for (let i = 0; i < commands[category].length; i++) {
-                if (string.includes(commands[category][i])) {
-                    // remove category from string
-                    string = string.replace(commands[category][i], '');
-                    return {type: category, request: string};
-                }
-            }
-        }
-        return false;
-    }
-
     loadJsonFile = (filename) => {
         let contents;
         const datadir = Gio.File.new_for_path(
