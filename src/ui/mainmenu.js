b8889431dd28623ce78c6d94480e3247acb303c7
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-30 14:13:44 -0300

    0.0.1

--- src/ui/mainmenu.js
+++ src/ui/mainmenu.js
@@ -1,5 +1,6 @@
 import St from 'gi://St';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 /**
  * @description main bar
