ece25308f9f1acad1d699b7742da8f26fffcabdc
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-30 00:03:10 -0300

    0.0.1

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -489,41 +489,4 @@ export class Utils {
 
         return adjustedRgbString;
     }
-
-    /**
-     * @description create tune string
-     * @param {string} [role='user'] type the role or leave empty for user role
-     * @returns {string} tune text
-     */
-    getTuneString(role = 'user') {
-        const date = new Date();
-        const location = this.app.userSettings.LOCATION;
-        // const user = `${_("Hi, I'm")} ${this.app.userSettings.USERNAME}. ${_("I'm from")} ${location} ${_('and today is')} ${date}`;
-        // const model = `${_('Hi! I am')} ${this.app.userSettings.ASSIST_NAME}. ${_('How can I help you today?')}`;
-        const user =
-            _("Hi, I'm") +
-            ' ' +
-            this.app.userSettings.USERNAME +
-            '.' +
-            ' ' +
-            _("I'm from") +
-            ' ' +
-            location +
-            ' ' +
-            _('and today is') +
-            ' ' +
-            date;
-        const model =
-            _('Hi! I am') +
-            ' ' +
-            this.app.userSettings.ASSIST_NAME +
-            '.' +
-            ' ' +
-            _('How can I help you today?');
-
-        if (role === 'user') {
-            return user;
-        }
-        return model;
-    }
 }
