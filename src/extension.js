6ab000a8a139b1ce4860d55a0150fd55f3b03414
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-11-30 00:03:01 -0300

    0.0.1

--- src/extension.js
+++ src/extension.js
@@ -216,8 +216,8 @@ const Aiva = GObject.registerClass(
                         this.extension.settings.set_string('location', country);
                         console.log('Location: ' + country);
                         this.database.addToHistory(
-                            this.utils.getTuneString('user'),
-                            this.utils.getTuneString('model'),
+                            this.gemini.getTuneString('user'),
+                            this.gemini.getTuneString('model'),
                         );
                         return country;
                     },
