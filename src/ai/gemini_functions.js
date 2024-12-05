2e36ebc83a8588f559bcdc423a7482ea263ec75c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-05 00:54:01 -0300

    0.0.1

--- src/ai/gemini_functions.js
+++ src/ai/gemini_functions.js
@@ -1,4 +1,6 @@
 import St from 'gi://St';
+import Soup from 'gi://Soup';
+import GLib from 'gi://GLib';
 import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 /**
@@ -395,33 +397,33 @@ export class GeminiFunctions {
             this.app.logError('Error opening site:', error);
             this.app.chat.editResponse(_('Error opening site'));
         }
+    }
 
-        _getWeather (location, response) {
-            try {
-                if (response) {
-                    this.app.chat.editResponse(response);
-                }
-                let _httpSession = new Soup.Session();
-                let message = Soup.Message.new('GET', url);
-                _httpSession.send_and_read_async(
-                    message,
-                    GLib.PRIORITY_DEFAULT,
-                    null,
-                    (_httpSession, result) => {
-                        let bytes = _httpSession.send_and_read_finish(result);
-                        let decoder = new TextDecoder('utf-8');
-                        response = decoder.decode(bytes.get_data());
-                        this.app.log(JSON.stringify(response));
-                        if (response) {
-                            this.app.chat.editResponse(response);
-                        }
-                    },
-                );
-            } catch (error) {
-                this.app.logError('Error getting weather:', error);
-                this.app.chat.editResponse(_('Error getting weather'));
+    _getWeather(location, response) {
+        try {
+            if (response) {
+                this.app.chat.editResponse(response);
             }
-
+            let url = '';
+            let _httpSession = new Soup.Session();
+            let message = Soup.Message.new('GET', url);
+            _httpSession.send_and_read_async(
+                message,
+                GLib.PRIORITY_DEFAULT,
+                null,
+                (_httpSession, result) => {
+                    let bytes = _httpSession.send_and_read_finish(result);
+                    let decoder = new TextDecoder('utf-8');
+                    response = decoder.decode(bytes.get_data());
+                    this.app.log(JSON.stringify(response));
+                    if (response) {
+                        this.app.chat.editResponse(location);
+                    }
+                },
+            );
+        } catch (error) {
+            this.app.logError('Error getting weather:', error);
+            this.app.chat.editResponse(_('Error getting weather'));
         }
     }
 }
