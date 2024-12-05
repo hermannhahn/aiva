834a27985a7028e1b3d54fb6358b12f7add1fd24
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-04 21:16:39 -0300

    0.0.1

--- src/ai/gemini_functions.js
+++ src/ai/gemini_functions.js
@@ -13,31 +13,72 @@ export class GeminiFunctions {
             _('read_lang_var1'),
             _('read_lang_var2'),
         ];
-        this.openAppActivation = [
-            _('open'),
-            _('open_lang_var1'),
-            _('open_lang_var2'),
-            _('run'),
-            _('run_lang_var1'),
-            _('run_lang_var2'),
-            _('execute'),
-            _('execute_lang_var1'),
-            _('execute_lang_var2'),
-            _('start'),
-            _('start_lang_var1'),
-            _('start_lang_var2'),
-            _('launch'),
-            _('launch_lang_var1'),
-            _('launch_lang_var2'),
-        ];
-        this.searchWebActivation = [
-            _('search'),
-            _('search_lang_var1'),
-            _('search_lang_var2'),
-            _('find'),
-            _('find_lang_var1'),
-            _('find_lang_var2'),
-        ];
+        (this._readClipboardDeclaration = {
+            name: 'read_clipboard',
+            description: _('To use when you want to read the clipboard'),
+            parameters: {
+                type: 'object',
+                properties: {
+                    response: {
+                        type: 'string',
+                        description:
+                            _('Response text before run, e.g.: ') +
+                            _('Sure, start reading...'),
+                    },
+                },
+                required: ['response'],
+            },
+        }),
+            (this.openAppActivation = [
+                _('open'),
+                _('open_lang_var1'),
+                _('open_lang_var2'),
+                _('run'),
+                _('run_lang_var1'),
+                _('run_lang_var2'),
+                _('execute'),
+                _('execute_lang_var1'),
+                _('execute_lang_var2'),
+                _('start'),
+                _('start_lang_var1'),
+                _('start_lang_var2'),
+                _('launch'),
+                _('launch_lang_var1'),
+                _('launch_lang_var2'),
+            ]);
+        (this._openAppDeclaration = {
+            name: 'open_app',
+            description:
+                _('To use when you want to open an application') +
+                ' [OS: Ubuntu 24.04.1 LTS]',
+            parameters: {
+                type: 'object',
+                properties: {
+                    commandLine: {
+                        type: 'string',
+                        description:
+                            _('Non-sudo command line to run, e.g.: ') +
+                            'firefox https://google.com',
+                    },
+                    response: {
+                        type: 'string',
+                        description:
+                            _('Response text before run, e.g.: ') +
+                            _('Sure, opening google...'),
+                    },
+                },
+                required: ['commandLine', 'response'],
+            },
+        }),
+            (this.searchWebActivation = [
+                _('search'),
+                _('search_lang_var1'),
+                _('search_lang_var2'),
+                _('find'),
+                _('find_lang_var1'),
+                _('find_lang_var2'),
+            ]);
+
         this.activationWords = [
             ...this.readClipboardActivation,
             ...this.openAppActivation,
@@ -69,61 +110,15 @@ export class GeminiFunctions {
     declarations(words) {
         let declarations = [];
         for (const word of words) {
-            if(this.readClipboardActivation.includes(word)) {
-                declarations.push(this._readClipboardDeclaration)
+            if (this.readClipboardActivation.includes(word)) {
+                declarations.push(this._readClipboardDeclaration);
+            }
+            if (this.openAppActivation.includes(word)) {
+                declarations.push(this._openAppDeclaration);
+            }
         }
-
-        let result = {
-            functionDeclarations: [
-                {
-                    name: 'open_app',
-                    description:
-                        _('To use when you want to open an application') +
-                        ' [OS: Ubuntu 24.04.1 LTS]',
-                    parameters: {
-                        type: 'object',
-                        properties: {
-                            commandLine: {
-                                type: 'string',
-                                description:
-                                    _('Non-sudo command line to run, e.g.: ') +
-                                    'firefox https://google.com',
-                            },
-                            response: {
-                                type: 'string',
-                                description:
-                                    _('Response text before run, e.g.: ') +
-                                    _('Sure, opening google...'),
-                            },
-                        },
-                        required: ['commandLine', 'response'],
-                    },
-                },
-            ],
-        };
-        return result;
+        return {declarations};
     }
-
-    _readClipboardDeclaration = 
-        {
-            name: 'read_clipboard',
-            description: _(
-                'To use when you want to read the clipboard',
-            ),
-            parameters: {
-                type: 'object',
-                properties: {
-                    response: {
-                        type: 'string',
-                        description:
-                            _('Response text before run, e.g.: ') +
-                            _('Sure, start reading...'),
-                    },
-                },
-                required: ['response'],
-            },
-        },
-
 
     _readClipboardText(response) {
         this.app.extension.clipboard.get_text(
