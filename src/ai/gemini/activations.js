b549eb142b4d44fd9c1cbaacb718330024ac4d7c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-12-07 13:26:08 -0300

    0.0.1

--- src/ai/gemini/activations.js
+++ src/ai/gemini/activations.js
@@ -1,40 +1,22 @@
-import St from 'gi://St';
 import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 /**
  * @description gemini functions
  * @param {object} app
  */
-export class FunctionsDeclarations {
+export class FunctionsActivations {
     constructor(app) {
         this.app = app;
 
-        this.toolsActivation = ['aiva', this.app.userSettings.ASSIST_NAME];
+        this.tools = ['aiva', this.app.userSettings.ASSIST_NAME];
 
-        this.readClipboardActivation = [
+        this.readClipboard = [
             _('read'),
             _('read_lang_var1'),
             _('read_lang_var2'),
         ];
 
-        this._readClipboardDeclaration = {
-            name: 'read_clipboard',
-            description: _('Clipboard Text-to-Speech'),
-            parameters: {
-                type: 'object',
-                properties: {
-                    response: {
-                        type: 'string',
-                        description:
-                            _('Response text before run, e.g.: ') +
-                            _('Sure, start reading...'),
-                    },
-                },
-                required: ['response'],
-            },
-        };
-
-        this.cmdActivation = [
+        this.cmd = [
             _('open'),
             _('open_lang_var1'),
             _('open_lang_var2'),
@@ -52,30 +34,7 @@ export class FunctionsDeclarations {
             _('launch_lang_var2'),
         ];
 
-        this._cmdDeclaration = {
-            name: 'command_line',
-            description:
-                _('Run non-sudo commandline in the user terminal') +
-                '. [OS: Ubuntu 24.04.1 LTS]',
-            parameters: {
-                type: 'object',
-                properties: {
-                    commandLine: {
-                        type: 'string',
-                        description: _('commandline to run'),
-                    },
-                    response: {
-                        type: 'string',
-                        description:
-                            _('Response text before run, e.g.: ') +
-                            _('Sure, opening GNOME Software...'),
-                    },
-                },
-                required: ['commandLine', 'response'],
-            },
-        };
-
-        this.browseWebActivation = [
+        this.browseWeb = [
             _('open'),
             _('open_lang_var1'),
             _('open_lang_var2'),
@@ -105,29 +64,7 @@ export class FunctionsDeclarations {
             _('access_lang_var2'),
         ];
 
-        this._browseWebDeclaration = {
-            name: 'browse_web',
-            description: _('Open url in the browser.'),
-            parameters: {
-                type: 'object',
-                properties: {
-                    url: {
-                        type: 'string',
-                        description:
-                            _('URL to open, e.g.: ') + 'https://google.com',
-                    },
-                    response: {
-                        type: 'string',
-                        description:
-                            _('Response text before open, e.g.: ') +
-                            _('Sure, opening...'),
-                    },
-                },
-                required: ['url'],
-            },
-        };
-
-        this.weatherActivation = [
+        this.weather = [
             _('weather'),
             _('weather_lang_var1'),
             _('forecast'),
@@ -186,28 +123,7 @@ export class FunctionsDeclarations {
             _('blizzard'),
         ];
 
-        this._weatherDeclaration = {
-            name: 'get_weather',
-            description: _('show the weather for today.'),
-            parameters: {
-                type: 'object',
-                properties: {
-                    location: {
-                        type: 'string',
-                        description:
-                            _(
-                                'Provide a location, e.g.: "New York, NY" (always use this format)',
-                            ) +
-                            ' ' +
-                            _(
-                                "or don't provide a location to show weather from user location",
-                            ),
-                    },
-                },
-            },
-        };
-
-        this.activationWords = [
+        this.words = [
             ...this.toolsActivation,
             ...this.weatherActivation,
             ...this.readClipboardActivation,
@@ -216,177 +132,5 @@ export class FunctionsDeclarations {
         ];
 
         console.log('Functions loaded.');
-    }
-
-    callback(command, args) {
-        command = command?.toLowerCase();
-        const commandLine = args.commandLine;
-        const response = args.response;
-        let location = args.location;
-        if (!location || location === undefined) {
-            location = this.app.userSettings.LOCATION;
-        }
-        const url = args.url;
-
-        switch (command) {
-            case 'read_clipboard':
-                this._readClipboardText(response);
-                break;
-            case 'command_line':
-                this._commandLine(commandLine, response);
-                break;
-            case 'browse_web':
-                this._browseWeb(url, response);
-                break;
-            case 'get_weather':
-                this._getWeather(location);
-                break;
-            default:
-                this.app.chat.editResponse(
-                    _("Sorry, I can't do that right now. Maybe in the future."),
-                );
-                break;
-        }
-    }
-
-    declarations(text) {
-        let functionDeclarations = [];
-        text = text.replace(/[.,!?;:"]/g, '');
-        const words = text.toLowerCase().split(' ');
-
-        for (const word of words) {
-            if (this.readClipboardActivation.includes(word)) {
-                functionDeclarations.push(this._readClipboardDeclaration);
-            }
-            if (this.browseWebActivation.includes(word)) {
-                functionDeclarations.push(this._browseWebDeclaration);
-            }
-            if (this.cmdActivation.includes(word)) {
-                functionDeclarations.push(this._cmdDeclaration);
-            }
-            if (this.weatherActivation.includes(word)) {
-                functionDeclarations.push(this._weatherDeclaration);
-            }
-        }
-        return [{functionDeclarations}];
-    }
-
-    _readClipboardText(response) {
-        this.app.extension.clipboard.get_text(
-            St.ClipboardType.CLIPBOARD,
-            (clipboard, result) => {
-                if (result) {
-                    let clipboardText = result;
-                    this.app.azure.tts(
-                        response +
-                            '\n ' +
-                            clipboardText +
-                            '\n ' +
-                            _('End of text!'),
-                    );
-                } else {
-                    this.app.chat.editResponse(
-                        _('Failed to get text from clipboard'),
-                    );
-                }
-            },
-        );
-    }
-
-    _commandLine(commandLine, response) {
-        try {
-            if (commandLine) {
-                const command = commandLine.split(' ')[0];
-
-                // check if app is installed
-                const isInstalled = this.app.utils.isAppInstalled(command);
-                if (!isInstalled) {
-                    this.app.chat.editResponse(
-                        _('Please, install') +
-                            ' ' +
-                            command +
-                            ' ' +
-                            _('first') +
-                            '.',
-                    );
-                    return;
-                }
-
-                if (response) {
-                    this.app.chat.editResponse(response);
-                }
-
-                this.app.utils.executeCommand(commandLine);
-                return;
-            }
-
-            this.app.chat.editResponse(response);
-        } catch (error) {
-            this.app.logError('Error opening site:', error);
-            this.app.chat.editResponse(_('Error opening site'));
-        }
-    }
-
-    _browseWeb(url, response) {
-        try {
-            // const msedgeIsInstalled = this.app.utils.isAppInstalled('msedge');
-            // if (msedgeIsInstalled) {
-            //     if (response) {
-            //         this.app.chat.editResponse(response);
-            //     }
-            //     this.app.utils.executeCommand(`msedge ${url}`);
-            //     return;
-            // }
-
-            const chromeIsInstalled =
-                this.app.utils.isAppInstalled('google-chrome');
-            if (chromeIsInstalled) {
-                if (response) {
-                    this.app.chat.editResponse(response);
-                }
-                this.app.utils.executeCommand(`google-chrome ${url}`);
-                return;
-            }
-
-            const operaIsInstalled = this.app.utils.isAppInstalled('opera');
-            if (operaIsInstalled) {
-                if (response) {
-                    this.app.chat.editResponse(response);
-                }
-                this.app.utils.executeCommand(`opera ${url}`);
-                return;
-            }
-
-            const firefoxIsInstalled = this.app.utils.isAppInstalled('firefox');
-            if (firefoxIsInstalled) {
-                if (response) {
-                    this.app.chat.editResponse(response);
-                }
-                this.app.utils.executeCommand(`firefox ${url}`);
-                return;
-            }
-
-            this.app.chat.editResponse(
-                _("You don't have any browser installed.") +
-                    ' ' +
-                    _('Please, install a browser and try again.') +
-                    ' ' +
-                    _('Opening App Store...'),
-            );
-            this.app.utils.executeCommand('gnome-software');
-        } catch (error) {
-            this.app.logError('Error opening site:', error);
-            this.app.chat.editResponse(_('Error opening site'));
-        }
-    }
-
-    _getWeather(location) {
-        try {
-            this.app.log('Getting weather from ' + location);
-            this.app.utils.getCurrentLocalWeather(location);
-        } catch (error) {
-            this.app.logError('Error getting weather:', error);
-            this.app.chat.editResponse(_('Error getting weather'));
-        }
     }
 }
